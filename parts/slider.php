85508987604a1bc27406f6dcd7b4d4f6c1074f27
Abubakr8911 <abubakr8911@gmail.com>
2019-03-17 10:04:42 +0200

    fix

--- parts/slider.php
+++ parts/slider.php
@@ -1,6 +1,6 @@
-<div class="jquery1-slider-wrap flexslider">
+<div class="">
    <ul class="slides">
-      <li class="jqslider karma-slider-normal-image">
+      <li class="">
          <!-- <div class="slider-content-main">
             <h2 class="slider-post-title">Personalize</h2>
             <p>Every aspect of Karma makes it super easy to personalize your website just the way you want it. We&#8217;ve worked tirelessly to ensure that Karma will continue to deliver the powerful options you&#8217;re after. With 30 preset color schemes, 6 premium sliders and over 100 glorious shortcodes Karma is the perfect choice for experts and beginners alike.</p>
