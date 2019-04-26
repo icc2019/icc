6c4808e566747b5071edb267c43d1a0800b1d061
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-04-26 17:21:59 +0300

    Update content.php

--- parts/content.php
+++ parts/content.php
@@ -1,15 +1,16 @@
 <?php //include('right-panel.php');?>
 <div class="col">
   <div class="content" >
-    <h1><strong>Inter​national Conference on Computing​ <span></strong>(ICC 2019 )<br /><span>October 22-24, 2019 - Riyadh, Saudi Arabia.
+    <h1><strong>Inter​national Conference on Computing​ <span></strong>(ICC 2019)<br /><span>October 22-24, 2019 - Riyadh, Saudi Arabia
   </span></h1>
   <h3>Welcome to ICC 2019
 </h3>
     <p>
-      In light of the KSA vision 2030 , Princess Norah University will hold the International Conference on Computing, in October 22-24 2019, Riyadh , KSA. ICC 2019, which is endorsed by The International Association for Sharing Knowledge and Sustainability (IASKS), brings  executives, professionals, educators, researchers, and practitioners from across the world to one hub that will foresee tomorrow's challenges today, suggesting solutions, trading ideas, and analyzing and discussing current developments in information technology within: Data Science, Cyber Security, Network & IOT, and Information Systems.
+      In light of the KSA vision 2030, Princess Norah University will hold the International Conference on Computing, in October 22-24 2019, Riyadh , KSA. ICC 2019, which is endorsed by The International Association for Sharing Knowledge and Sustainability (IASKS), brings  executives, professionals, educators, researchers, and practitioners from across the world to one hub that will foresee tomorrow's challenges today, suggesting solutions, trading ideas, and analyzing and discussing current developments in information technology within: Data Science, Cyber Security, Network & IOT, and Information Systems.
 
     <p>
-      ICC 2019 invites authors to submit original contributions in the event research area. ICC  2019 covers four main themes: Data Science, Cyber Security, Networks and IoT and Information System and Applications. Papers accepted and presented at ICC 2019, will be submitted for possible inclusion in Springer Lecture Note in Computer Science (LNCS).
+      ICC 2019 invites authors to submit original contributions in the event research area. ICC  2019 covers four main themes: Data Science, Cyber Security, Networks and IoT and Information System and Applications. ICC 2019 accepted papers will be submitted for possible inclusion in Springer Communications in Computer and Information Science (CCIS). This distinguished conference proceedings publishes the latest research developments in all areas of computer science at a high level. Communications in Computer and Information Science (CCIS) series is indexed by the ISI Conference Proceedings Citation Index - Science (CPCI-S), included in ISI Web of Science, EI Engineering Index (Compendex and Inspec databases), ACM Digital Library, dblp, Google Scholar, Scopus, etc. 
+
 
     </p>
   </div>
