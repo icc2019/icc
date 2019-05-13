405102d2125b6f851c10ba45d8e5b8c2ba02656c
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-05-13 20:22:07 +0300

    Update submission.php

--- parts/submission.php
+++ parts/submission.php
@@ -16,8 +16,13 @@ Communications in Computer and Information Science (CCIS) series is indexed by t
   </p>
 
 
-  <h1>Submission Information</h1>
-  <p>
+  <h1>Instructions to Authors</h1>
+  
+      <p>
+    The submission processes will be managed by easychair.org. If you have used this system before, you can use the same username and password. If this is your first time using EasyChair, you will need to register for an account by clicking “I have no EasyChair account” button. Upon completion of registration, you will get a notification email from the system and you are ready for submitting your paper. Further instructions about the submission guidelines and templates will be provided soon.
+
+  </p>
+      <p>
     <p>
       ICC invites two types of submissions which should be structured as technical papers:
     </p>
@@ -34,10 +39,7 @@ Communications in Computer and Information Science (CCIS) series is indexed by t
     </p>
     </li>
   </ul>
-  <p>
-    The submission processes will be managed by easychair.org. If you have used this system before, you can use the same username and password. If this is your first time using EasyChair, you will need to register for an account by clicking “I have no EasyChair account” button. Upon completion of registration, you will get a notification email from the system and you are ready for submitting your paper. Further instructions about the submission guidelines and templates will be provided soon.
-
-  </p>
+  
   <p>
     ICC uses a double-blind review process. Each submission will be reviewed by at least three program committee members. Every peer-reviewed research article will have a DOI assigned, and indexed in the Conference Proceedings Citation Index (CPCI).
 
