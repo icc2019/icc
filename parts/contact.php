d0a4bfee4fac6daf3eb4ccacb636046513aa532f
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-05-14 02:58:00 +0300

    Update contact.php

--- parts/contact.php
+++ parts/contact.php
@@ -1,6 +1,5 @@
 <div class="">
-    <p>Please feel free to contact us by </p>
+    <p>For any inqiry please contact us</p>
     <p>(email: <a href="mailto:ccis-nw-Inquiry@pnu.edu.sa"><b>ccis-nw-Inquiry@pnu.edu.sa</b></a>)</p>
-    <p> if you require any further assistance or information.</p>
     <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
 </div>
