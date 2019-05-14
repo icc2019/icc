83739babb9cabac832c1772d92ac46a570557ca0
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-05-14 03:07:15 +0300

    Update Steering-Commitee-content.php

--- parts/Steering-Commitee-content.php
+++ parts/Steering-Commitee-content.php
@@ -7,20 +7,19 @@
   <ul class="">
 <ol>
     <h1>Steering Committee </h1>
-</ol>
-
-<ol> Atta Badii, <strong>University of Reading, UK</strong></ol>
- <ol>Danny Hughes, <strong>K. U. Leuven, Belgium</strong></ol>
-<ol>David Taniar, <strong>Monash University, Australia </strong></ol>
-<ol> Mai Alduailij, <strong>Princess Norah University, KSA</strong></ol>
- <ol>Haroon Malik, <strong>Marshall University, USA</strong></ol>
-<ol>Heba A. Kurdi, <strong> King Saud University </strong></ol>
-<ol>Rahim Rahmani,  <strong>Stockholm University, Sweden</strong></ol>
- <ol>Shiroq Al-Megren, <strong> Massachusetts Institute of Technology (MIT), USA</strong></ol>
-<ol>Stephane Galland, <strong>   UTBM, France</strong></ol>
-<ol> Maali Alabdulhafith, <strong>Princess Norah University, KSA</strong></ol>
-<ol>  Vincenzo Loia, <strong>University of Salerno, Italy</strong></ol>
- <ol> Zakaria Maamar, <strong>Zayed University, UAE</strong></ol>
+<p>
+    Atta Badii, <b>University of Reading, UK</b>
+ <br> Danny Hughes, <b>K. U. Leuven, Belgium</b>
+ <br>David Taniar, <b>Monash University, Australia </b>
+ <br>Mai Alduailij, <b>Princess Norah University, KSA</b>
+ <br>Haroon Malik, <b>Marshall University, USA</b>
+ <br>Heba A. Kurdi, <b> King Saud University </b>
+ <br>Rahim Rahmani,  <b>Stockholm University, Sweden</b>
+ <br>Shiroq Al-Megren, <b> Massachusetts Institute of Technology (MIT), USA</b>
+ <br>Stephane Galland, <b>   UTBM, France</strong></ol>
+ <br> Maali Alabdulhafith, <b>Princess Norah University, KSA</b>
+ <br>  Vincenzo Loia, <b>University of Salerno, Italy</b>
+ <br> Zakaria Maamar, <b>Zayed University, UAE</b>
 
 </ul>
 </div>
