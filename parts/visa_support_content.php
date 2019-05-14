6e8a5d54cab48a39bd899a7f67bf971525f05077
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-05-15 02:48:13 +0300

    Update visa_support_content.php

--- parts/visa_support_content.php
+++ parts/visa_support_content.php
@@ -5,10 +5,20 @@
   <ol>
     <h1> Visa Support </h1>
 <p>
-  
-  <p>
-    Visa support information will be available soon
+ 
+  <b> How to Apply for a Saudi Visa </b> <br>
+
+The Saudi Arabia visa application form will only take a few minutes to complete. To avoid delays and possible rejections, make sure that all the sections are answered fully and accurately.
+
+You are only required to fill in the information stated in English. The information stated in Arabic are to be filled by the Princess Nourah University only. 
+
+Once you have your form ready, please send it to the following email ccis-nw-Inquiry@pnu.edu.sa. We will complete your form and submit your application. 
+
+Once the Saudi application form has been submitted and processed, the approved Visa application will be sent to you by email. To obtain your visa, you have to show up in person in the Saudi Embassy in your country with a copy of your approved application and the with the corresponding passport. 
+
   </p>
+    
+    
 <br /><br /><br /><br /><br /><br /><br /><br />
 </div>
 
