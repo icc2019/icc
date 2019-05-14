254efb35a70796d9a7e8e369163ee77df89b845c
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-05-15 02:52:47 +0300

    Update visa_support_content.php

--- parts/visa_support_content.php
+++ parts/visa_support_content.php
@@ -3,12 +3,11 @@
   
   
   <ol>
-    <h1> Visa Support </h1> <br>
-<p>
- 
-  <b> How to Apply for a Saudi Visa </b> <br> 
+    <h1> Visa Support </h1> 
+    
+<p> <b> How to Apply for a Saudi Visa </b> </p>
 
-The Saudi Arabia visa application form will only take a few minutes to complete. To avoid delays and possible rejections, make sure that all the sections are answered fully and accurately.
+<p> The Saudi Arabia visa application form will only take a few minutes to complete. To avoid delays and possible rejections, make sure that all the sections are answered fully and accurately.
 
 You are only required to fill in the information stated in English. The information stated in Arabic are to be filled by the Princess Nourah University only. 
 
