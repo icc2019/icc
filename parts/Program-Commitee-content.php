7dba212fb9cac1c5f6a816a612f17c2a657a1349
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-05-14 07:03:06 +0300

    Update Program-Commitee-content.php

--- parts/Program-Commitee-content.php
+++ parts/Program-Commitee-content.php
@@ -9,22 +9,23 @@
     <h1>Program Committee </h1>
 </ol>
 
-      <p>
-Abdulhadi Alqarni, <b>Jubail University College, KSA</b>
-          <br>Eiman Kanjo, <b>Nottingham Trent University, UK</b>
+<p>Abdulhadi Alqarni, <b>Jubail University College, KSA</b>
+<br>Azman Shamsuddin, <b>University Sains Malaysia, Malaysia </b>
+<br>Eiman Kanjo, <b>Nottingham Trent University, UK</b>
 <br>Faouzi Zarai, <b>School of Electronics and Telecommunications of Sfax, Tunisia</b>
 <br>Hala Mokhtar, <b>Saud University, KSA</b>
 <br>Khaoula Hamdi, <b>King Saud University, KSA</b>
 <br>Lamia Albraheem, <b>King Saud University, KSA</b>
 <br>Lina Altoaimy, <b>King Saud University, KSA</b>
 <br>Maha Aljohani, <b>University of Jeddah, KSA</b>
-<br>Maher Elshakankiri, <b>University of Regina, Regina, Canada</b> 
+<br>Maher Elshakankiri, <b>University of Regina, Canada</b> 
 <br>Maysoon Abulkhair, <b>King Abdulaziz University, KSA</b>
 <br>Mohamed Buhari, <b>King Abdulaziz University, KSA</b>
 <br>Monia Hamdi, <b>Gabès University, Tunisia</b>
 <br>Mznah Al-Rofdhaan, <b>King Saud University, KSA</b>
 <br>Norah Alghamdi, <b>Princess Norah University, KSA</b>
 <br>Ridwan Qureshi, <b>King Abdulaziz University, KSA</b>
+<br>Rosni Abdullah, <b>University Sains Malaysia, Malaysia </b>
 <br>Shaimaa Elsaid, <b>Zagazig University, Egypt</b>
 <br>Sherif Kamel Hussein, <b>Arab East Colleges for high studies, KSA</b>
 <br>Soha S. Zaghloul Mekki, <b>King Saud University, KSA</b>
