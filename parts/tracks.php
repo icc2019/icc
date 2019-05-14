8219f28a9012b0b54f1c2e6b134cb079d30f5b28
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-05-14 03:51:36 +0300

    Update tracks.php

--- parts/tracks.php
+++ parts/tracks.php
@@ -15,19 +15,19 @@
                           </ul>
                           <div id="tab-3" class="tab-box ui-tabs-panel ui-widget-content ui-corner-bottom" aria-labelledby="ui-id-4" role="tabpanel" aria-expanded="false" aria-hidden="true" style="display: none;">
                              <p> <ul>
-                               <li><p>Big Data and Analytics</p></li>
-                               <li><p>Bioinformatics</p></li>
-                               <li><p>Distributed Systems</p></li>
-                               <li><p>Artificial Intelligence</p></li>
-                               <li><p>Data/Web Mining</p></li>
-                               <li><p>Data visualization</p></li>
-                               <li><p>Data classification and taxonomy</p></li>
-                               <li><p> Data metrics and metrology</p></li>
-                               <li><p> Machine learning and deep learning</p></li>
-                               <li><p> Data analytics</p></li>
-                               <li><p>Data provenance</p></li>
-                               <li><p>Big Data, cloud computing and data-intensive systems</p></li>
-                               <li><p>Innovative data-intensive applications such as health, energy, transport, food, soil and water, etc.</p></li><br />
+                               Big Data and Analytics
+                               <br>Bioinformatics
+                               <br>Distributed Systems
+                               <br>Artificial Intelligence
+                               <br>Data/Web Mining
+                               <br>Data visualization
+                               <br>Data classification and taxonomy
+                               <br>Data metrics and metrology
+                               <br>Machine learning and deep learning
+                               <br>Data analytics
+                               <br>Data provenance
+                               <br>Big Data, cloud computing and data-intensive systems
+                               <br>Innovative data-intensive applications such as health, energy, transport, food, soil and water, etc.<br />
                              </ul></p>
                           </div>
                           <div id="tab-4" class="tab-box ui-tabs-panel ui-widget-content ui-corner-bottom" aria-labelledby="ui-id-5" role="tabpanel" aria-expanded="false" aria-hidden="true" style="display: none;">
