89a5fabaab29fd2bdaded13464eda05fb63056be
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-05-14 03:40:31 +0300

    Update Program-Commitee-content.php

--- parts/Program-Commitee-content.php
+++ parts/Program-Commitee-content.php
@@ -10,6 +10,27 @@
 </ol>
 
 <ol> TBD</ol>
+      <p>
+Abdulhadi Alqarni, <b>College of Computer Science and Engineering at Jubail University College, KSA</b>
+          <br>Eiman Kanjo, <b>Nottingham Trent University, UK</b>
+<br>Faouzi Zarai, <b>School of Electronics and Telecommunications of Sfax, Tunisia</b>
+<br>Hala Mokhtar, <b>Saud University, KSA</b>
+<br>Khaoula Hamdi, <b>King Saud University, KSA</b>
+<br>Lamia Albraheem, <b>King Saud University, KSA</b>
+<br>Lina Altoaimy, <b>King Saud University, KSA</b>
+<br>Maha Aljohani, <b>University of Jeddah, KSA</b>
+<br>Maher Elshakankiri, <b>University of Regina, Regina, Canada</b> 
+<br>Maysoon Abulkhair, <b>King Abdulaziz University, KSA</b>
+<br>Mohamed Buhari, <b>King Abdulaziz University, KSA</b>
+<br>Monia Hamdi, <b>Gabès University, Tunisia</b>
+<br>Mznah Al-Rofdhaan, <b>King Saud University, KSA</b>
+<br>Ridwan Qureshi, <b>King Abdulaziz University, KSA</b>
+<br>Shaimaa Elsaid, <b>Zagazig University, Egypt</b>
+<br>Sherif Kamel Hussein, <b>Arab East Colleges for high studies, KSA</b>
+<br>Soha S. Zaghloul Mekki, <b>King Saud University, KSA</b>
+<br>Yasser Dakroury, <b>Ain Shams University-Cairo, Egypt</b>
+
+
 
 </ul>
 </div>
