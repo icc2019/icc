cd913aca4c73fe5a2d9821ecfa91a3105c25781b
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-05-14 03:14:55 +0300

    Update local-Organizing-Commitee-content.php

--- parts/local-Organizing-Commitee-content.php
+++ parts/local-Organizing-Commitee-content.php
@@ -14,17 +14,17 @@
 </ol>
 <p>
       Dua Nassar <b>Princess Nourah University</b>
-    <br>Evon Abu-Taieh<b>Jordan University</b>
+    <br>Evon Abu-Taieh<b> Jordan University</b>
     <br>Ghadah Aldehim<b> Princess Nourah University </b>
-    <br>Hadil Ahmed Shaiba<b>Princess Nourah University </b>
+    <br>Hadil Ahmed Shaiba<b> Princess Nourah University </b>
     <br>Hanan Aljuaid<b> Princess Nourah University</b>
-    <br>Hanen Karamti<b>Princess Nourah University </b>
-    <br>Heba A. Kurdi<b>King Saud University </b>
+    <br>Hanen Karamti<b> Princess Nourah University </b>
+    <br>Heba A. Kurdi<b> King Saud University </b>
     <br>Hessah Aleisa<b> Princess Nourah University</b>
     <br>Mada Alaskar<b> Princess Nourah University </b>
-    <br>Manel AYADI<b>Princess Nourah University </b>
-    <br>Myriam Hadjouni<b>Princess Nourah University </b>
-    <br>Samiah Chelloug<b>Princess Nourah University </b>
+    <br>Manel AYADI<b> Princess Nourah University </b>
+    <br>Myriam Hadjouni<b> Princess Nourah University </b>
+    <br>Samiah Chelloug<b> Princess Nourah University </b>
 
 </ul>
 </div>
