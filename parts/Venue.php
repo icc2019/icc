fc3dc6ad539a48cea2074a065db680d7457661c7
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-05-16 00:53:42 +0300

    Update Venue.php

--- parts/Venue.php
+++ parts/Venue.php
@@ -5,11 +5,11 @@
     <br><br>
   <p>  Conference and Convention Center </p>
 
-  <p> Princess Nourah bint Abdulrahman University</p>
+    <p> <b> Princess Nourah bint Abdulrahman University </b> </p>
 
-  <p> Riyadh, Kingdom Of Saudi Arabia (KSA)​.</p>
+    <p> <b> Riyadh, Kingdom Of Saudi Arabia (KSA)​. </b> </p>
 
-   <p>PO Box 84428</p>
+    <p> <b> PO Box 84428 </b> </p>
      <!-- Map Section -->
      <section class="map-section">
           <!--Map Canvas-->
@@ -19,7 +19,7 @@
       <!--End Map Section -->
 
    <p>
-     Please <a href="https://www.google.com/maps/place/3.4.0+-+Conference+and+Convention+Center/@24.8435119,46.7194638,15z/data=!4m5!3m4!1s0x3e2efb8bbb535085:0x1670bc0af61f7278!8m2!3d24.8419901!4d46.7254858" target="_blank">(Click here)</a>​ for location map and directions​
+     Please <b> <a href="https://www.google.com/maps/place/3.4.0+-+Conference+and+Convention+Center/@24.8435119,46.7194638,15z/data=!4m5!3m4!1s0x3e2efb8bbb535085:0x1670bc0af61f7278!8m2!3d24.8419901!4d46.7254858" target="_blank">Click here</a>​ </b> for location map and directions​
    </p>
   </div>
   </div>
