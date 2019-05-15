9c3e8b1bb47f87d12b4ef5344410234b74e123d7
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-05-16 00:58:37 +0300

    Update About_Riyadh_slider.php

--- parts/About_Riyadh_slider.php
+++ parts/About_Riyadh_slider.php
@@ -187,7 +187,7 @@ img {vertical-align: middle;}
                               <h4 class="team-member-name">Kingdom Centre Tower (Al-Mamlaka Tower)</h4>
                               <p class="team-member-title"> </p>
 
-  								 <p>
+  								 <p align="justify">
   									 Kingdom Center Tower is located at 2239 Al Urubah Rd, Al Olaya, Riyadh 12214-9597, Saudi Arabia.
   			 Kingdom Tower is one of the best Saudi Shopping Malls. Within the tower there are not only numerous stores but also the Four Season Hotel, apartments, offices, the Sky Bridge and Spazio Restaurant. Kingdom Centre Tower is among the most visited areas by tourists.
   				
@@ -221,7 +221,7 @@ img {vertical-align: middle;}
                               <h4 class="team-member-name">Al Masmak Museum</h4>
 
 
-  														<p>
+  														<p align="justify">
   															Al Masmak Fortress is a well known historic landmark here in Saudi Arabia. On June 11, 1995, His Royal Highness Prince Salman bin Abdul Aziz opened the Al Masmak Historical Museum, which tells the story of the Kingdom’s unification and establishment by King Abdul Aziz.  Full of rich history, Al Masmak Museum is commonly visited by tourists and history enthusiasts. This renovated 19th-century mud fortress looks like a giant sandcastle, and contains costumes, handicrafts, a sitting room, a courtyard and a well.
   														
                                 <br>
@@ -257,7 +257,7 @@ img {vertical-align: middle;}
                               <h4 class="team-member-name">Al Bujairi Heritage Park</h4>
 
 
-  														<p>
+  														<p align="justify">
   															Al Bujairi Heritage Park is a huge historical theme park. It contains various restaurants and cafes. Many people visit Al Bujairi Park to have picnics, chat with friends and relax under the trees. If you are looking for a nice place to go for a walk or read a book in a relaxing and fun place in Riyadh, this would be it. The best part is it’s open 24 hours a day! You can visit Al Bujairi Heritage Park whenever you would like, although it is particularly lively during the weekends.
   													  <p><b>Visiting hours and location:</b></p>
   														<p><b>Hours: open 24 hours a day</b></p>
@@ -293,7 +293,7 @@ img {vertical-align: middle;}
 
 
 
-  															<p>
+  															<p align="justify">
 
   														The King Abdullah park is one of the most popular areas to visit here in Riyadh. It is a large park with a lot of greenery and places to sit down and relax.  There is also an activity area for young children and a skating area for teenagers. However, the star attraction of the park is its musical fountain. King Abdullah Park is the perfect place to relax at and enjoy nature.
   														 </p>
@@ -333,7 +333,7 @@ img {vertical-align: middle;}
                               <h4 class="team-member-name">
   National Museum of Saudi Arabia </h4>
 
-  <p>
+  <p align="justify">
   	The National Museum of Riyadh is the most famous museum in the Kingdom of Saudi Arabia. Located in the middle part of King Abdul-Aziz Historical Center, which was established to be a cultural and civilization center highlighting the prominent history of the Arabian Peninsula. It is a museum that aims to introduce the KSA history with its role and fundamentals. The museum consists of 10 galleries, courtyards and a section that houses fancy cars and other belongings of the founding king of Saudi Arabia.
   </p>
   													  <p><b>For information regarding visiting hours and location visit: </b></p>
@@ -376,7 +376,7 @@ img {vertical-align: middle;}
   															Tuwaiq Mountains (The Edge of The World)
    </h4>
 
-  <p>
+  <p align="justify">
   	A series of mountains located 35 kilometers away from Riyadh, running approximately 800 km from the southern border of Al-Qasim in the north, to the northern edge of the Empty Quarter desert near Wadi ad-Dawasir in the south. The Tuwaiq mountains have become popularly known as ‘the edge of the world’ for how similar they are to the US’s Grand Canyon in Arizona. It is a perfect place for hiking and nature lovers.
   </p>
                              
