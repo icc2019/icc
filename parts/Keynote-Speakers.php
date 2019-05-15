71778014905e4c14790743e06764e64e21ccdc5b
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-05-16 01:03:54 +0300

    Update Keynote-Speakers.php

--- parts/Keynote-Speakers.php
+++ parts/Keynote-Speakers.php
@@ -13,7 +13,7 @@
 							<h4 class="team-member-name">Prof. Muhammad Kamal Youcef-Toumi</h4>
 
 							<p >Biography:</p>
-							<p>
+							<p align="justify">
 								Kamal Youcef-Toumi is a Professor in the Mechanical Engineering Department at Massachusetts Institute of Technology(MIT) USA. He is
 	 Co-Director of the Center for Complex Engineering Systems at KACST and MIT, and Director of
 	 the Ibn Khaldun Fellowship for Saudi Women. His M.S. and Sc.D. degrees are from MIT.
@@ -43,7 +43,7 @@
 	                         <div class="member-bio">
 	                            <h4 class="team-member-name">Prof. Omer Rana</h4>
 	                            <p>Biography:</p>
-															<p>
+															<p align="justify">
 																Omer F. Rana is Professor of Performance Engineering at Cardiff University, with research interests in high performance distributed computing, data analysis/mining and multi-agent systems. He is the Dean of International for the Physical Science and Engineering College at Cardiff University He previously led the Complex Systems Research Group, and was also formerly the deputy director of the Welsh eScience Centre and had the opportunity to interact with a number of computational scientists across Cardiff University and the UK. He serves on the steering committee of Cardiff University's multi-disciplinary "Data Innovation" and "Energy Systems" Research Institutes. Rana has contributed to specification and standardisation activities via the Open Grid Forum and worked as software developer with London-based Marshall Bio-Technology Limited prior to joining Cardiff University, where he developed specialist software to support biotech instrumentation. He also contributed to public understanding of science, via the Wellcome Trust funded "Science Line", in collaboration with BBC and Channel 4. Rana holds a PhD in "Neural Computing and Parallel Architectures" from Imperial College (London Univ.), an MSc in Microelectronics (Univ. of Southampton) and a BEng in Information Systems Eng. from Imperial College (London Univ.). He serves on the editorial boards (as Associate Editor) of IEEE Transactions on Parallel and Distributed Systems, (formerly) IEEE Transactions on Cloud Computing, IEEE Cloud Computing magazine and ACM Transactions on Internet Technology. He is a founding-member and associate editor of ACM Transactions on Autonomous & Adaptive Systems.
 
 															</p>
@@ -64,7 +64,7 @@
 							<div class="member-bio">
 								 <h4 class="team-member-name">Prof. Muhammad Khurram Khan</h4>
 								 <p>Biography:</p>
-								 <p>
+								 <p align="justify">
 								Prof. Muhammad Khurram Khan is currently working at the Center of Excellence in Information Assurance (CoEIA), King Saud University, Kingdom of Saudi Arabia. He is the founder and CEO of the ‘Global Foundation for Cyber Studies and Research’ (http://www.gfcyber.org), which is an independent, non-profit, and non-partisan cybersecurity think tank based in Washington D.C.
 							He is the Editor-in-Chief of a well-reputed International journal ‘Telecommunication Systems’ published by Springer with its recent impact factor of 1.542 (JCR 2017). Furthermore, he is the editor of several international journals, including, IEEE Communications Surveys & Tutorials, IEEE Communications Magazine, IEEE Internet of Things Journal, IEEE Transactions on Consumer Electronics, IEEE Access, IEEE Consumer Electronics Magazine, Journal of Network & Computer Applications, PLOS ONE, Electronic Commerce Research, and IET Wireless Sensor Systems.
 							Prof. Khurram is the recipient of King Saud University Award for Scientific Excellence (Research Productivity) in May 2015. He is also a recipient of King Saud University Award for Scientific Excellence (Inventions, Innovations, and Technology Licensing) in May 2016.
@@ -92,7 +92,7 @@
 																	<div class="member-bio">
 																		 <h4 class="team-member-name">Prof. Stephanie E. August</h4>
 																		 <p >Biography: </p>
-																		 <p>
+																		 <p align="justify">
 																		 Stephanie E. August is a Professor of Computer Science in the Department of Electrical Engineering and Computer Science in the Frank R. Seaver College of Science and Engineering at Loyola Marymount University in Los Angeles. She is currently serving as a Program Director in the Directorate for Education and Human Resources Division of Undergraduate Education at the National Science Foundation in Arlington, Virginia, in the United States. At NSF she works on programs related to undergraduate education, data science, the future of work, and the future of STEM education. She received her B.A. in Slavic Languages and M.S. and Ph.D. in Computer Science, all from the University of California, Los Angeles. Prior to LMU she was a staff engineer at Hughes Aircraft Company and a copy cataloger at UCLA. Stephanie has served as department director of graduate studies and special assistant to the chief academic officer for graduate studies.  Her current research interests focus on online interactive digital learning environments and infusing other disciplines with computing concepts. She is also interested in computational models of reasoning by analogy and exploring the boundaries between people and machines. Her teaching repertoire includes courses in databases, artificial intelligence, new media (English+graphics+computing), programming 3D animation tools (animation+computing), and first year seminars (English composition+computing). Stephanie enjoys hiking, camping, gardening and baking, and looks forward to the day when a machine can read a novel and understand the symbolism in it.
 
 																		 </p>
@@ -110,7 +110,7 @@
 															 					<div class="member-bio">
 															 						 <h4 class="team-member-name">Dr. Norma Alias</h4>
 															 						 <p>Biography: </p>
-																					 <p>
+																					 <p align="justify">
 																					 Dr Norma Alias is currently an Associate Professor, and Research Fellow and Head of Networking and Linkages Division of Center for Sustainable Nanomaterials, IbnuSina Institute for Scientific and Industrial Research (IIS), UniversitiTeknologi Malaysia (UTM). She was appointed as an Associate Professor and Research Fellow at King Saud University, Saudi Arabia. She is a member of the Malaysian Mathematical Sciences Society.
 															 AP Dr. Norma possessed a PhD degree in Industrial Computing (Parallel Computing) in 2004. She has a diverse field of specializations including mathematical modeling, big data simulation, industrial computing, scientific computing, high performance computing, shared-distributed parallel computing system, grid computing and software development. She has supervised for 13 PhD students and 32 MSc with Philosophy. She is supervising ongoing 5 PhD students , 12 MSc with Philosophy students, 2 postdoctoral students and reviewing postgraduate students of local and international universities. Awarded as Distinguished Women in Mathematical Sciences in the year 2018 from VIWA, India. AP Dr. Norma has a distinguished academic and research track record having published over 200 publications which consist of index journal, index proceedings articles, books and modules. She is an active innovator , having earned 2 product patent disclosures in 2015, 4 Intellectual property declarations, and 16 medals won in product innovation and invention expo since 2004 and having 2 products for commercialization.
 															 She is active organizing a monthly workshop such as Matlab, Maple, Mathematica, Comsol Multiphysics, CUDA, C++, PVM, MPI, OpenMP, Big data analytics, FEM, FDM and Machine Learning. She has been invited by 14 international conferences in the year 2016-2017 as keynote, plenary and invited speaker in Malaysia, China, India, New Zealand, New Zealand, South Korea,  Japan, Indonesia, Vietnam, Laos, Saudi Arabia, Kuwait, United Arab Emirates and Pakistan.
