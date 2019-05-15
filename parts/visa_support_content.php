75f60c51935b905f5096823caca4e02816fc7489
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-05-16 02:50:57 +0300

    Update visa_support_content.php

--- parts/visa_support_content.php
+++ parts/visa_support_content.php
@@ -12,7 +12,7 @@
 
 You are only required to fill in the information stated in English. The information stated in Arabic are to be filled by Princess Nourah University only. 
 
-Once you have your form ready, please send it to the following email <b> <a href="mailto:ccis-nw-Inquiry@pnu.edu.sa" target="_top"> ccis-nw-Inquiry@pnu.edu.sa </a> </b>. We will complete your form and submit your application. 
+Once you have your form ready, please send it to the following email <b> <a href="mailto:ccis-nw-Inquiry@pnu.edu.sa" target="_top"> ccis-nw-Inquiry@pnu.edu.sa </a> </b>, with the email subject “Visa Letter: Paper number”, where paper number is the number you received when you submitted your paper to easychair. We will complete your form and submit your application. 
 
 Once the Saudi application form has been submitted and processed, the approved Visa application will be sent to you by email. To obtain your visa, you have to show up in person in the Saudi Embassy in your country with a copy of your approved application and passport. 
 
