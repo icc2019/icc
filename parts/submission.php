82a3d19b75391069bf215e0f797cd1f18783c70e
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-05-16 00:45:03 +0300

    Update submission.php

--- parts/submission.php
+++ parts/submission.php
@@ -4,12 +4,12 @@
   <div class="Organizing-Committee">
     <h1>  Submission</h1>
     <div class="">
-  <p>
+  <p align="justify">
 
 
 International Conference on Computing (ICC) invites authors to submit original contributions in the event research area. ICC covers four main themes: Data Science, Cyber Security, Networks and IoT and Information System and Applications.
 
-  <p>
+  <p align="justify">
    ICC 2019 accepted papers will be submitted for possible inclusion in Springer Communications in Computer and Information Science (CCIS). This distinguished conference proceedings publishes the latest research developments in all areas of computer science at a high level. 
 Communications in Computer and Information Science (CCIS) series is indexed by the ISI Conference Proceedings Citation Index - Science (CPCI-S), included in ISI Web of Science, EI Engineering Index (Compendex and Inspec databases), ACM Digital Library, dblp, Google Scholar, Scopus, etc.
 
@@ -20,7 +20,7 @@ Communications in Computer and Information Science (CCIS) series is indexed by t
   <b>Instructions to Authors</b>
       </p>
       
-      <p>
+      <p align="justify">
         
 Submitted papers should be within the scope of the ICC 2019 conference. All contributions must be of high quality, original, should not have been published elsewhere and should not be submitted for publication elsewhere during the review period or time of the conference.
 The conference proceedings will be published by Springer-Verlag in Springer's CCIS (Communications in Computer and Information Science) Book Series. When formatting papers, please refer to the Springer-Verlag web site, and strictly follow the Instructions to Authors.
@@ -47,14 +47,14 @@ Papers are required in PDF format for review purposes, but authors are required 
       
     <p>  
       <b>Publication process</b>
-      <p> The language of publication is exclusively English. Authors publishing in CCIS have to sign the Springer CCIS copyrighttransfer form, however, they are free to use their material published in CCIS for substantially changed, more elaborate subsequent publications elsewhere. For the preparation of the camera-ready papers/files, authors have to strictly adhere to the Springer CCIS Authors' Instructions and are strongly encouraged to use the CCIS LaTeX style files or templates, as described<b> <a href="https://www.springer.com/gp/computer-science/lncs/conference-proceedings-guidelines?countryChanged=true" target="_blank"> here </a> </b>. </p>
+      <p align="justify"> The language of publication is exclusively English. Authors publishing in CCIS have to sign the Springer CCIS copyrighttransfer form, however, they are free to use their material published in CCIS for substantially changed, more elaborate subsequent publications elsewhere. For the preparation of the camera-ready papers/files, authors have to strictly adhere to the Springer CCIS Authors' Instructions and are strongly encouraged to use the CCIS LaTeX style files or templates, as described<b> <a href="https://www.springer.com/gp/computer-science/lncs/conference-proceedings-guidelines?countryChanged=true" target="_blank"> here </a> </b>. </p>
       </p>
     <br>
     
     <p>
     
       <b>Abstracting/Indexing</b>
-    <p> CCIS is abstracted/indexed in DBLP, Google Scholar, EI-Compendex, Mathematical Reviews, SCImago, Scopus. CCIS volumes are also submitted for the inclusion in ISI Proceedings.  </p>
+    <p align="justify"> CCIS is abstracted/indexed in DBLP, Google Scholar, EI-Compendex, Mathematical Reviews, SCImago, Scopus. CCIS volumes are also submitted for the inclusion in ISI Proceedings.  </p>
     </p>
   <br>
         
@@ -65,11 +65,11 @@ Papers are required in PDF format for review purposes, but authors are required 
   </p>
       <br>
 
-      <p>
+      <p align="justify">
       <b>Copyright form (after your paper has been accepted)</b>
-<p>  Each contribution must be accompanied by a Springer copyright form, a so-called 'Consent to Publish' form. <b> <a href="assests/Contract_Book_Contributor_Consent_to_Publish_CCIS_SIP.pdf" target="_blank" download>Download the copyright form </a> </b>. </p>
+<p align="justify">  Each contribution must be accompanied by a Springer copyright form, a so-called 'Consent to Publish' form. <b> <a href="assests/Contract_Book_Contributor_Consent_to_Publish_CCIS_SIP.pdf" target="_blank" download>Download the copyright form </a> </b>. </p>
 
-<p> Modified forms are not acceptable. Authors will be asked to transfer the copyright of the paper to the Springer. This will ensure the widest possible protection and dissemination of information under copyright laws. One author may sign on behalf of all of the authors of a particular paper. In this case, the author signs for and accepts responsibility for releasing the material on behalf of all co-authors. </p> 
+<p align="justify"> Modified forms are not acceptable. Authors will be asked to transfer the copyright of the paper to the Springer. This will ensure the widest possible protection and dissemination of information under copyright laws. One author may sign on behalf of all of the authors of a particular paper. In this case, the author signs for and accepts responsibility for releasing the material on behalf of all co-authors. </p> 
 </p>
        
         
