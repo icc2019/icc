082181527c9cdcc0226470ee02db2af953bf13d3
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-05-15 16:24:56 +0300

    Update submission.php

--- parts/submission.php
+++ parts/submission.php
@@ -26,7 +26,7 @@ Submitted papers should be within the scope of the ICC 2019 conference. All cont
 The conference proceedings will be published by Springer-Verlag in Springer's CCIS (Communications in Computer and Information Science) Book Series. When formatting papers, please refer to the Springer-Verlag web site, and strictly follow the Instructions to Authors.
 
 <br> <br>
-Papers are required in PDF format for review purposes, but authors are required to upload editable word-processor files (  <a href="https://www.dropbox.com/s/0bf2vryzd7zsfpv/Latex.zip?dl=0" target="_blank"> LaTeX  or  <a href="https://www.dropbox.com/s/w7iylzuxct97twt/Word.zip?dl=0" target="_blank"> --> MS Word ) at the end of the review process. Please find the correct template options below.
+Papers are required in PDF format for review purposes, but authors are required to upload editable word-processor files (  <a href="https://www.dropbox.com/s/0bf2vryzd7zsfpv/Latex.zip?dl=0" target="_blank"> LaTeX <!-- </a> --> or  <a href="https://www.dropbox.com/s/w7iylzuxct97twt/Word.zip?dl=0" target="_blank"> --> MS Word <!-- </a> --> ) at the end of the review process. Please find the correct template options below.
 
 <br> <br>
         <b> ICC 2019 invites two types of submissions which should be structured as technical papers </b>
