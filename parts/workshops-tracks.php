43bc247e7e029158d65665794741563c17dbff00
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-05-22 22:16:32 +0300

    Update workshops-tracks.php

--- parts/workshops-tracks.php
+++ parts/workshops-tracks.php
@@ -99,7 +99,7 @@
     <p>
 
       
-      You can submit a  <b> <a href="assests/Abstract2214.docx" target="_blank" download> workshop proposal </a> </b> as a single  PDF file online via the following email:<a href="mailto:ccis-cs-workshop@pnu.edu.sa"><strong>ccis-cs-workshop@pnu.edu.sa</strong></a>
+      You can submit a   <a href="assests/Abstract2214.docx" target="_blank" download> workshop proposal </a>  as a single  PDF file online via the following email:<a href="mailto:ccis-cs-workshop@pnu.edu.sa"><strong>ccis-cs-workshop@pnu.edu.sa</strong></a>
    
     
     </p>
