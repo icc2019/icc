957cd6ee7b9dedb181106ba8ee33eabd8cd260e7
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-05-26 06:48:43 +0300

    Update workshops-tracks.php

--- parts/workshops-tracks.php
+++ parts/workshops-tracks.php
@@ -6,7 +6,7 @@
   <p align="justify">
     The International Conference in Computing (ICC) will host a number of workshops. Each workshop will be led by one presenter/group of presenters for the duration of the session time, which may be half or full day. The aim of the conference workshops is to provide training session for emerging topics not specifically covered in the main conference. Workshops should highlight trend topics related to Data Science, IoT, Cyper Security, and Networks. An invitation is open to submit workshop proposals.
   </p>
-  <h1>WORKSHOP TOPICS</h1>
+
   <br/>
   <p align="justify">
   <b>The topics for the workshops are under for trends as follows:</b>
@@ -19,71 +19,7 @@
                              <li class="ui-state-default ui-corner-top  col-lg-2 col-xs-12 col-sm-12" role="tab" tabindex="-1" aria-controls="tab-5" aria-labelledby="ui-id-6" aria-selected="false"><a href="#tab-5" class="tab ui-tabs-anchor" role="presentation" tabindex="-1" id="ui-id-6"><span>  NETWORK</span></a></li>
                              <li class="ui-state-default ui-corner-top  col-lg-2 col-xs-12 col-sm-12 " role="tab" tabindex="-1" aria-controls="tab-6" aria-labelledby="ui-id-7" aria-selected="false"><a href="#tab-6" class="tab ui-tabs-anchor" role="presentation" tabindex="-1" id="ui-id-7"><span>INTERNET OF THINGS (IOT)</span></a></li>
                           </ul>
-                          <div id="tab-3" class="tab-box ui-tabs-panel ui-widget-content ui-corner-bottom" aria-labelledby="ui-id-4" role="tabpanel" aria-expanded="false" aria-hidden="true" style="display: none;">
-                             <p> <ul>
-                               <li><p>Deep Learning</p></li>
-                               <li><p>Machine Learning</p></li>
-                               <li><p>Predictive Analytics</p></li>
-                               <li><p>Natural Language Processing</p></li>
-                               <li><p>Data Visualization</p></li>
-                               <li><p>Data Wrangling</p></li>
-                               <li><p>AI Research</p></li>
-                               <li><p>Data Science at Scale</p></li>
-                               <li><p>Data Science Management</p></li>
-                               <li><p>Data Science Workflows</p></li>
-                               <li><p>Recommendation Systems</p></li>
-                               <li><p>Transfer Learning</p></li>
-                               <li><p>Conversational AI</p></li>
-                               <li><p>Speech Recognition</p></li>
-                               <li><p>Unsupervised Learning</p></li>
-                               <li><p>Image Classification</p></li>
-                               <li><p>Machine translation</p></li>
-                             </ul></p>
-                          </div>
-                          <div id="tab-4" class="tab-box ui-tabs-panel ui-widget-content ui-corner-bottom" aria-labelledby="ui-id-5" role="tabpanel" aria-expanded="false" aria-hidden="true" style="display: none;">
-                             <p> <ul>
-                               <li> <p>Authentication and Authorization</p></li>
-                               <li> <p>Communication and Network Security</p></li>
-                               <li> <p>Security Testing and Verification</p></li>
-                               <li> <p>Socio-technical Security</p></li>
-                               <li> <p>Reverse Engineering</p></li>
-                               <br/>
-                             </ul></p>
-                          </div>
-                          <div id="tab-5" class="tab-box ui-tabs-panel ui-widget-content ui-corner-bottom" aria-labelledby="ui-id-6" role="tabpanel" aria-expanded="false" aria-hidden="true" style="display: none;">
-                             <p> <ul>
-                               <li> <p>Software defined networks.</p></li>
-                               <li> <p>Network virtualization.</p></li>
-                               <li> <p>Vehicular and 5G networks.</p></li>
-                               <li> <p>Cloud networking.</p></li>
-                               <li> <p>Network data analytics.</p></li>
-                               <li> <p>Smart cites.</p></li>
-                               <li> <p>Wireless networks.</p></li>
-                               <li> <p>Mobile networks.</p></li>
-                               <li> <p>Satellite networks.</p></li>
-                               <li> <p>Optical networks.</p></li>
-                               <li> <p>Multimedia networks.</p></li>
-                               <br/>
-
-                             </ul></p>
-                          </div>
-                          <div id="tab-6" class="tab-box ui-tabs-panel ui-widget-content ui-corner-bottom" aria-labelledby="ui-id-7" role="tabpanel" aria-expanded="true" aria-hidden="false" style="display: block;">
-                             <p><ul>
-                               <li> <p> Internet of Things Architecture</p></li>
-                               <li>  <p>Big data and IoT Data Analytics </p></li>
-                               <li>  <p>IoT and Artificial Intelligence techniques</p></li>
-                               <li>  <p>IoT Best Practices, Standards and OpenSource</p></li>
-                               <li>  <p>Web Technologies for the IoT.</p></li>
-                               <li>  <p>Description and Discovery of IoT devices, including mobile and moving devices.</p></li>
-                               <li>  <p>Real World Applications of IoT technology</p></li>
-                               <li>  <p>Green IoT</p></li>
-                               <li>  <p>The Internet of People</p></li>
-                               <br/>
-                             </ul></p>
-                          </div>
-                       </div>
-                    </div>
-
+                   
   <!--  -->
   <div class="Organizing-Committee">
 
