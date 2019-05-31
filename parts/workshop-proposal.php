cfe11a6b9411dbd701aa956de068fa8817388033
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-05-31 05:12:16 +0300

    Update workshop-proposal.php

--- parts/workshop-proposal.php
+++ parts/workshop-proposal.php
@@ -182,13 +182,59 @@
 
 
 
-
+<!--
   <h1>Please address all questions to the ICC Workshops Chairs:</p></h1>
   <ul>
     <ol><p> Chair Dr. Norah Alghamdi</p></ol>
   <ol><p>Deputy Chair: Dr. Dua Nassar​</p></ol>
   <ol><p>Contact: <a href="mailto:ccis-cs-workshop@pnu.edu.sa"><strong>ccis-cs-workshop@pnu.edu.sa</strong></a></p></ol>
   </ul>
+
+-->
+    
+    
+    <h1>WORKSHOP PROPOSAL SUBMISSION</h1>
+    
+   
+    <p>
+
+      <b> You can submit a  workshop proposal as a single  PDF file online via the following email:<a href="mailto:ccis-cs-workshop@pnu.edu.sa"><strong>ccis-cs-workshop@pnu.edu.sa</strong></a> </b>
+    
+    </p>
+  </div>
+    
+    <br> 
+  <table>
+     
+    <tr> 
+      <td> <p>​  Download the workshop proposal format </p> </td>
+      <td> <p> <b> <a href="assests/Abstract2214.docx" target="_blank"download> here </a> </b> </p> </td>
+    </tr>
+  
+  
+    <tr>
+     <td><p>​The deadline for workshop abstract submission </p></td>
+      <td><p>June 6th , 2019</p></td>
+    </tr>
+    <tr>
+      <td><p>Announcement of acceptance/rejection </p></td>
+      <td><p>July 15th, 2019</p></td>
+    </tr>
+    <tr>
+      <td><p>Deadline for final presentation submission</p></td>
+      <td><p>August 1st, 2019</p></td>
+    </tr>
+  </table>
+    
+    <bR>
+
+  <ul>
+  <ol><p><b>Please address all questions to the ICC Workshops Chairs:</b></p></ol>
+  <ol><p>Chair Dr. Norah Alghamdi</p></ol>
+  <ol><p>Deputy Chair Dr. Dua Nassar​</p></ol>
+  <ol><p>Contact <a href="mailto:ccis-cs-workshop@pnu.edu.sa"><strong>ccis-cs-workshop@pnu.edu.sa</strong></a></p></ol>
+  </ul>
+  </div>
 
     </div>
   </div>
