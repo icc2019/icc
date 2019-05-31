81f8c1c882237c96153120de0e8ae51146150686
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-05-31 05:20:23 +0300

    Update header.php

--- parts/header.php
+++ parts/header.php
@@ -1,6 +1,6 @@
 
 <div>
-  <img src="assests/images/slider/Banner.jpg" style="max-height: 400px;
+  <img src="assests/images/slider/Banner.jpg" style="max-height: 600px;
     width: 100%;
     object-fit: initial;" />
   <!-- <div class="headercontent" >
