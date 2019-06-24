a9d1b2e011472d6c61548d190278b189002936a4
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-06-24 03:43:40 +0300

    Update visa_support_content.php

--- parts/visa_support_content.php
+++ parts/visa_support_content.php
@@ -8,15 +8,6 @@
     
 <p> <h3><b> How to Apply for a Saudi Visa (Visitors)</b></h3> </p>
 
-<p align="justify"> The Saudi Arabia visa application <b> <a href="assests/نموذج طلب إصدار وتمديد تأشيرة زيارة حكومية بالترويسة الجديدة.docx" target="_blank" download> form </a> </b> will only take a few minutes to complete. To avoid delays and possible rejections, make sure that all the sections are answered fully and accurately.
-
-You are only required to fill in the information stated in English. The information stated in Arabic are to be filled by Princess Nourah University only. 
-
-Once you have your form ready, please send it to the following email <b> <a href="https://visa.mofa.gov.sa/" target="_top"> visa.mofa.gov.sa</a></b>, with the email subject “Visa Letter: Paper number”, where paper number is the number you received when you submitted your paper to easychair. We will complete your form and submit your application. 
-
-Once the Saudi application form has been submitted and processed, the approved Visa application will be sent to you by email. To obtain your visa, you have to show up in person in the Saudi Embassy in your country with a copy of your approved application and passport. 
-
-  </p>
   <p>
     To apply for your visa, please go to the <a href="https://visa.mofa.gov.sa/" target="_top">link</a> </b> , or visit the Saudi
 Embassy or Consulate in your city, or the nearest Saudi
