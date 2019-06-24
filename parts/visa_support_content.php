885b2710d7528057f46c3f8a63f122512241f954
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-06-24 03:37:44 +0300

    Update visa_support_content.php

--- parts/visa_support_content.php
+++ parts/visa_support_content.php
@@ -6,7 +6,7 @@
     <h1> Visa Support </h1>
     <br>
     
-<p> <b> How to Apply for a Saudi Visa </b> </p>
+<p> <h3><b> How to Apply for a Saudi Visa (Visitors)</b></h3> </p>
 
 <p align="justify"> The Saudi Arabia visa application <b> <a href="assests/نموذج طلب إصدار وتمديد تأشيرة زيارة حكومية بالترويسة الجديدة.docx" target="_blank" download> form </a> </b> will only take a few minutes to complete. To avoid delays and possible rejections, make sure that all the sections are answered fully and accurately.
 
@@ -17,7 +17,14 @@ Once you have your form ready, please send it to the following email <b> <a href
 Once the Saudi application form has been submitted and processed, the approved Visa application will be sent to you by email. To obtain your visa, you have to show up in person in the Saudi Embassy in your country with a copy of your approved application and passport. 
 
   </p>
-    
+  <p>
+    To apply for your visa, please go to  <a href="https://visa.mofa.gov.sa/" target="_top">the link</a> </b> , or visit the Saudi
+Embassy or Consulate in your city, or the nearest Saudi
+representative office to find out more about Saudi visa issuance.
+If you are an author and you would like a letter indicating the
+acceptance of your paper, please send an email to:<b> <a href="mailto:ccis-nw-Inquiry@pnu.edu.sa" target="_top"> ccis-nw-Inquiry@pnu.edu.sa </a> </b>with your name, passport information and your
+accepted paper ID.
+    </p>
     
 <br /><br /><br /><br /><br /><br /><br /><br />
 </div>
