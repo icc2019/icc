028edce10ef005a2928684fc73479ad92deb875f
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-07-10 23:09:15 +0300

    Update Registration.php

--- parts/Registration.php
+++ parts/Registration.php
@@ -5,6 +5,47 @@
    Registration</h1>
    <p>
 
+     <table style="width:100%">
+  <tr>
+    <th>  </th>
+    <th>Early Registration <br> (August 22nd )</th>
+    <th>Early Registration <br> (August 22nd )</th> 
+    <th>Onsite Registration <br> (Begins October 22nd)</th>
+  </tr>
+  
+  <tr>
+    <td>Non-Author</td>
+    <td><center>300 $</td>
+    <td><center>350 $</td>
+    <td><center>400 $</td>
+  </tr>
+  <tr>
+    <td>Author</td>
+    <td><center>250 $</td>
+    <td><center>300 $</td>
+    <td><center>350 $</td>
+  </tr>
+  <tr>
+    <td>Student</td>
+    <td><center>100 SR</td>
+    <td><center>150 SR</td>
+    <td><center>200 SR</td>
+  </tr>
+   <tr>
+    <td>PNU Student</td>
+    <td><center>50 SR</td>
+    <td><center>100 SR</td>
+    <td><center>150 SR</td>
+  </tr>
+   <tr>
+    <td>Workshops Only</td>
+    <td><center>100 $</td>
+    <td><center>150 $</td>
+    <td><center>200 $</td>
+  </tr>
+  
+</table>
+     
   Registration site and fees will be available soon​
    </p>
    <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
