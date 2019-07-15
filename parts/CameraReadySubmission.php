9e98e571a22b99c9e2e0acf03cc4e4bde19b37c5
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-07-15 15:29:00 +0300

    Update CameraReadySubmission.php

--- parts/CameraReadySubmission.php
+++ parts/CameraReadySubmission.php
@@ -23,8 +23,8 @@ Any changes you see fit to make to the final version of your paper as well as su
 &#9679; Ensure that your paper conforms to the appropriate submission types, 12-15 pages for long papers and 6-8 pages for short papers, including all tables, figures, and references.
 &#9679; Ensure that all tables and figures are at a high resolution.
 &#9679; If you have supplemental material, please read the following: Supplemental material can be divided into two types: appendices and non-readable supplemental material: <br>
-&#9675;	Appendices are material that can be read, and include lemmas, formulas, proofs, and tables that are not critical to the reading and understanding of the paper. In your final camera-ready paper, appendices come after the references in the main paper (see the Springer templates). Appendices do not count towards the page limit. <br>
-&#9675;	Non-readable supplemental material should follow the Springer templates. <br>
+&nbsp; &nbsp; &nbsp; &#9675;	Appendices are material that can be read, and include lemmas, formulas, proofs, and tables that are not critical to the reading and understanding of the paper. In your final camera-ready paper, appendices come after the references in the main paper (see the Springer templates). Appendices do not count towards the page limit. <br>
+&nbsp; &nbsp; &nbsp; &#9675;	Non-readable supplemental material should follow the Springer templates. <br>
 &#9679; The file (Camera Ready copy) must be in Portable Document Format (PDF) on A4 paper. <br>
 &#9679; All figures and tables must be included directly in the paper in their final positions, and at a resolution that is both readable when printed or when viewed on an electronic device in PDF format.<br>
 <br>
