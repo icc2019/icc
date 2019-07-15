56a170976ad14d98fed2e7eb676a06e24b09d8cd
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-07-15 16:04:36 +0300

    Update CameraReadySubmission.php

--- parts/CameraReadySubmission.php
+++ parts/CameraReadySubmission.php
@@ -20,12 +20,12 @@
     <br>
 
 The deadline is August 1st, 2019. Failure to submit your Camera-Ready files by this date will result in your paper being not included in the proceeding. You may submit the final version of your paper by navigating <b> <a href="https://easychair.org/account/signin" target="_blank"> HERE </a> </b> and following the instructions.
-<br>
+<br> <br>
     
-Any changes you see fit to make to the final version of your paper as well as suggestions made by the reviewers should be incorporated in the camera-ready version. In addition, please be sure to do all of the following:<br>
+Any changes you see fit to make to the final version of your paper as well as suggestions made by the reviewers should be incorporated in the camera-ready version. In addition, please be sure to do all of the following:<br> <br>
 &#9679;	Ensure that your paper conforms to the required format (see the Springer templates). <br>
 &#9679; Include the authors' names and affiliations under the title. Note that the list of authors should be identical to the list specified when submitting the paper.<br>
-&#9679; Ensure that your paper conforms to the appropriate submission types, 12-15 pages for long papers and 6-8 pages for short papers, including all tables, figures, and references.
+&#9679; Ensure that your paper conforms to the appropriate submission types, 12-15 pages for long papers and 6-8 pages for short papers, including all tables, figures, and references.<br>
 &#9679; Ensure that all tables and figures are at a high resolution. <br>
 &#9679; If you have supplemental material, please read the following: Supplemental material can be divided into two types: appendices and non-readable supplemental material: <br>
 &nbsp; &nbsp; &nbsp; &#9675;	Appendices are material that can be read, and include lemmas, formulas, proofs, and tables that are not critical to the reading and understanding of the paper. In your final camera-ready paper, appendices come after the references in the main paper (see the Springer templates). Appendices do not count towards the page limit. <br>
