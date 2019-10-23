513c0c288501a83d73e29c41fcae9e763a2f9f42
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-10-24 01:49:39 +0300

    Update Steering-Commitee-content.php

--- parts/Steering-Commitee-content.php
+++ parts/Steering-Commitee-content.php
@@ -8,19 +8,14 @@
 <ol>
     <h1>Steering Committee </h1>
 <p>
-    Atta Badii, <b>University of Reading, UK</b>
- <br> Danny Hughes, <b>K. U. Leuven, Belgium</b>
- <br>David Taniar, <b>Monash University, Australia </b>
- <br>Haroon Malik, <b>Marshall University, USA</b>
+   
  <br>Heba A. Kurdi, <b> King Saud University, KSA </b>
      <br>Mai Alduailij, <b>Princess Nourah University, KSA</b>
     <br> Maali Alabdulhafith, <b>Princess Nourah University, KSA</b>
     <br>Hadil Ahmed Shaiba, <b> Princess Nourah University, KSA </b>
- <br>Rahim Rahmani,  <b>Stockholm University, Sweden</b>
+
  <br>Shiroq Al-Megren, <b> Massachusetts Institute of Technology (MIT), USA</b>
- <br>Stephane Galland, <b>   UTBM, France</b>
- <br>  Vincenzo Loia, <b>University of Salerno, Italy</b>
- <br> Zakaria Maamar, <b>Zayed University, UAE</b>
+ 
 
 </ul>
 </div>
