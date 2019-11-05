5031ba46d9f96f239ac1521fcb0dbbfadb6887f1
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-11-05 20:54:46 +0300

    Update Sponsors-and-Partners.php

--- parts/Sponsors-and-Partners.php
+++ parts/Sponsors-and-Partners.php
@@ -31,12 +31,12 @@
 
 <div class="column">
 <a target="_blank" href="https://www.citrix.com/about/what-does-citrix-do.html?utm_medium=Paid+Search+(SEM)&utm_source=sem-bra-brand-em-en-sea-go&utm_term=brand+evergreen&utm_content=brand-think&ef_id=Cj0KCQiA2ITuBRDkARIsAMK9Q7PX0xVpwmSbsnSagH3HERI2a5R03okOnMsBEndkQs52Zg4GhqYA6YEaAgdbEALw_wcB:G:s&gclid=Cj0KCQiA2ITuBRDkARIsAMK9Q7PX0xVpwmSbsnSagH3HERI2a5R03okOnMsBEndkQs52Zg4GhqYA6YEaAgdbEALw_wcB">
-<img src="assests/images/sponsers/Citrix1 (1).png" alt="CITRIX" style="width:100%">   
+<img src="assests/images/sponsers/Citrix1 (1).png" alt="CITRIX" height="200" width="400">   
 </div>
     
 <div class="column">
 <a target="_blank" href="https://www.hpe.com/emea_europe/en/home.html">
-<img src="assests/images/sponsers/HPE1 (1).png" alt="HPE" style="width:100%">
+<img src="assests/images/sponsers/HPE1 (1).png" alt="HPE" height="200" width="400">
 </div>
 
 </div>
