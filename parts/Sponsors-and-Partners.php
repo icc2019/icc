09baaff8b7bc815fd7174e5e31ae7fcab514af11
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-11-05 18:43:46 +0300

    Update Sponsors-and-Partners.php

--- parts/Sponsors-and-Partners.php
+++ parts/Sponsors-and-Partners.php
@@ -1,53 +1,58 @@
-<div class="container ">
-<div class="row"  style="text-align: -webkit-center;">
-<ul class="" style="margin: 0 auto;
+    <div class="container ">
+    <div class="row"  style="text-align: -webkit-center;">
+   
+        
+    <ul class="" style="margin: 0 auto;
     display: grid;
     list-style: none;">
-      <li >
 
+    
+    <li >
+    <div class="slider-content-sub">
+    <a href="https://consumer.huawei.com/sa-en/"  target="_blank">
+    <img src="assests/images/sponsers/huawei-icon.svg" alt="" width="400px" height="200px"/>
+    <h3>HUAWEI</h3></a>
+    </div>
+    </li>
+    
+    
+         <li class="jqslider karma-slider-normal-image ">
          <div class="slider-content-sub">
-           <a href="https://consumer.huawei.com/sa-en/
-"  target="_blank">
-            <img src="assests/images/sponsers/huawei-icon.svg" alt="" width="400px" height="200px"/>
-            <h3>HUAWEI</h3></a>
+         <a href="https://www.dellemc.com/ar-sa/index.htm" target="_blank">
+         <img src="assests/images/sponsers/dellemc.svg" alt=""width="400px" height="200px" />
+         <h3>DEL EMC</h3></a>
          </div>
-
-      </li>
-      <li class="jqslider karma-slider-normal-image ">
-
+         </li>
+    
+    
+    <li class="jqslider karma-slider-normal-image" >
+    <div class="slider-content-sub">
+    <a href="http://www.aecl.com/en/Home" target="_blank">
+    <img src="assests/images/sponsers/aec.svg" alt=""width="400px" height="200px" />
+    <h3>AEC</h3></a>
+    </div>
+    </li>
+    
+         <li class="jqslider karma-slider-normal-image" >
          <div class="slider-content-sub">
-           <a href="https://www.dellemc.com/ar-sa/index.htm
-" target="_blank">
-            <img src="assests/images/sponsers/dellemc.svg" alt=""width="400px" height="200px" />
-          <h3>DEL EMC
-</h3></a>
+         <a href="https://www.cisco.com/c/en_sa/index.html"  target="_blank">
+         <img src="assests/images/sponsers/cisco.svg" alt=""width="400px" height="200px" />
+         <h3>CISCO</h3></a>
          </div>
+         </li>
+  
+    
+    <li class="jqslider karma-slider-normal-image" >
+    <div class="slider-content-sub">
+    <a href="https://www.orange-business.com/en"  target="_blank">
+    <img src="assests/images/sponsers/Orange_logo.svg" alt=""width="400px" height="200px" />
+    <h3>Orange</h3> </a>
+    </div>
+    </li>
 
-      </li>
-      <li class="jqslider karma-slider-normal-image" >
+     
 
-         <div class="slider-content-sub">
-           <a href="http://www.aecl.com/en/Home
-" target="_blank">
-            <img src="assests/images/sponsers/aec.svg" alt=""width="400px" height="200px" />
-          <h3>AEC</h3></a>
-         </div>
-         <div class="slider-content-sub">
-           <a href="https://www.cisco.com/c/en_sa/index.html
-"  target="_blank">
-            <img src="assests/images/sponsers/cisco.svg" alt=""width="400px" height="200px" />
-          <h3>CISCO</h3></a>
-         </div>
-         <div class="slider-content-sub">
-           <a href="https://www.orange-business.com/en
-       "  target="_blank">
-            <img src="assests/images/sponsers/Orange_logo.svg" alt=""width="400px" height="200px" />
-           <h3>Orange</h3> </a>
-         </div>
-
-      </li>
-
-   </ul>
-</div>
-</div>
+  </ul>
+  </div>
+  </div>
 
