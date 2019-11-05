06089487b917b03501743fbcef2bedb0cd8f56aa
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-11-05 22:21:01 +0300

    Update Sponsors-and-Partners.php

--- parts/Sponsors-and-Partners.php
+++ parts/Sponsors-and-Partners.php
@@ -12,9 +12,9 @@
         <p> <b> Gold Sponsors </b> </p>
 <br>
 <br>
-<!--<a target="_blank" href="https://www.cisco.com/c/en_sa/index.html">-->
+<a target="_blank" href="https://www.cisco.com/c/en_sa/index.html">
 <img src="assests/images/sponsers/CISCO.png" alt="CISCO" height="100" width="200">
-        <p> <b> <a href="https://www.cisco.com/c/en_sa/index.html" target="_top">CISCO</a> </b> </p>
+        </a>
 
 <br>
 <br>
@@ -27,8 +27,8 @@
 <p> <b> Silver Sponsors </b> </p>
 <br>
 <br>
-<!--<a target="_blank" href="https://www.citrix.com/about/what-does-citrix-do.html?utm_medium=Paid+Search+(SEM)&utm_source=sem-bra-brand-em-en-sea-go&utm_term=brand+evergreen&utm_content=brand-think&ef_id=Cj0KCQiA2ITuBRDkARIsAMK9Q7PX0xVpwmSbsnSagH3HERI2a5R03okOnMsBEndkQs52Zg4GhqYA6YEaAgdbEALw_wcB:G:s&gclid=Cj0KCQiA2ITuBRDkARIsAMK9Q7PX0xVpwmSbsnSagH3HERI2a5R03okOnMsBEndkQs52Zg4GhqYA6YEaAgdbEALw_wcB">-->
-<img src="assests/images/sponsers/Citrix1 (1).png" alt="CITRIX" height="70" width="200"> 
+<a target="_blank" href="https://www.citrix.com/about/what-does-citrix-do.html?utm_medium=Paid+Search+(SEM)&utm_source=sem-bra-brand-em-en-sea-go&utm_term=brand+evergreen&utm_content=brand-think&ef_id=Cj0KCQiA2ITuBRDkARIsAMK9Q7PX0xVpwmSbsnSagH3HERI2a5R03okOnMsBEndkQs52Zg4GhqYA6YEaAgdbEALw_wcB:G:s&gclid=Cj0KCQiA2ITuBRDkARIsAMK9Q7PX0xVpwmSbsnSagH3HERI2a5R03okOnMsBEndkQs52Zg4GhqYA6YEaAgdbEALw_wcB">
+    <img src="assests/images/sponsers/Citrix1 (1).png" alt="CITRIX" height="70" width="200"> </a>
  &nbsp;
  &nbsp;
  &nbsp;
@@ -37,8 +37,8 @@
 &nbsp;
         
     
-<!--<a target="_blank" href="https://www.hpe.com/emea_europe/en/home.html">-->
-<img src="assests/images/sponsers/HPE1 (1).png" alt="HPE" height="100" width="200">
+<a target="_blank" href="https://www.hpe.com/emea_europe/en/home.html">
+    <img src="assests/images/sponsers/HPE1 (1).png" alt="HPE" height="100" width="200"> </a>
 &nbsp;
 &nbsp; 
 &nbsp;
@@ -46,8 +46,8 @@
 &nbsp; 
 &nbsp;
  
-<!--<a target="_blank" href="https://www.hpe.com/emea_europe/en/home.html">-->
-<img src="assests/images/sponsers/NUTANIX.png" alt="NUTANIX" height="50" width="200">
+<a target="_blank" href="https://www.nutanix.com/go/what-is-nutanix-hyperconverged-infrastructure?utm_source=google_adwords&utm_medium=paid_search&utm_campaign=Google_Search_DG_Alpha_Brand_SAU&utm_term=nutanix&gclid=Cj0KCQiA2ITuBRDkARIsAMK9Q7M-Lf_v2MToAxmKXGykI_XqJewLbxiGPOHfsoTG0V5a2x192rHTTIwaAg1TEALw_wcB">
+    <img src="assests/images/sponsers/NUTANIX.png" alt="NUTANIX" height="50" width="200"> </a>
         
  <br>
 <br>
@@ -60,8 +60,8 @@
 <p> <b> Other Sponsors </b> </p>
 <br>
 <br>
-<!--<a target="_blank" href="https://www.citrix.com/about/what-does-citrix-do.html?utm_medium=Paid+Search+(SEM)&utm_source=sem-bra-brand-em-en-sea-go&utm_term=brand+evergreen&utm_content=brand-think&ef_id=Cj0KCQiA2ITuBRDkARIsAMK9Q7PX0xVpwmSbsnSagH3HERI2a5R03okOnMsBEndkQs52Zg4GhqYA6YEaAgdbEALw_wcB:G:s&gclid=Cj0KCQiA2ITuBRDkARIsAMK9Q7PX0xVpwmSbsnSagH3HERI2a5R03okOnMsBEndkQs52Zg4GhqYA6YEaAgdbEALw_wcB">-->
-<img src="assests/images/sponsers/HUAWEI.png" alt="HUAWEI" height="150" width="150"> 
+<a target="_blank" href="https://consumer.huawei.com/sa-en/">
+    <img src="assests/images/sponsers/HUAWEI.png" alt="HUAWEI" height="150" width="150"> </a> 
  &nbsp;
  &nbsp;
  &nbsp;
@@ -70,8 +70,8 @@
 &nbsp;
         
     
-<!--<a target="_blank" href="https://www.hpe.com/emea_europe/en/home.html">-->
-<img src="assests/images/sponsers/DELL EMC.png" alt="DELL EMC" height="60" width="200">
+<a target="_blank" href="https://www.dellemc.com/ar-sa/index.htm">
+    <img src="assests/images/sponsers/DELL EMC.png" alt="DELL EMC" height="60" width="200"> </a>
 &nbsp;
 &nbsp; 
 &nbsp;
@@ -79,8 +79,8 @@
 &nbsp; 
 &nbsp;
  
-<!--<a target="_blank" href="https://www.hpe.com/emea_europe/en/home.html">-->
-<img src="assests/images/sponsers/AEC.png" alt="AEC" height="160" width="170">
+<a target="_blank" href="http://www.aecl.com/en/Home">
+    <img src="assests/images/sponsers/AEC.png" alt="AEC" height="160" width="170"> </a>
 &nbsp;
 &nbsp; 
 &nbsp;
@@ -88,9 +88,9 @@
 &nbsp; 
 &nbsp;
         
-<!--<a target="_blank" href="https://www.hpe.com/emea_europe/en/home.html">-->
+<a target="_blank" href="https://www.orange-business.com/en">
 <img src="assests/images/sponsers/Orange_logo.svg" alt="Orange" height="150" width="150">
-       
+    </a>
 <br>
 <br>
 <br>
