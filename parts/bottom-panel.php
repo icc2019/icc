5ab38cb86d0c728b915b928734b853dd0b4c7d44
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-11-06 15:09:26 +0300

    Update bottom-panel.php

--- parts/bottom-panel.php
+++ parts/bottom-panel.php
@@ -36,9 +36,20 @@
     &nbsp;
     &nbsp;
     &nbsp;
+      &nbsp;
+      &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
     <a target="_blank" href="https://www.cisco.com/c/en_sa/index.html">
 <img src="assests/images/sponsers/cisco.svg" alt="CISCO" height="80" width="80" ali>
-      &nbsp;
+      <br>
+      <br>
       
     <a target="_blank" href="https://www.citrix.com/about/what-does-citrix-do.html?utm_medium=Paid+Search+(SEM)&utm_source=sem-bra-brand-em-en-sea-go&utm_term=brand+evergreen&utm_content=brand-think&ef_id=Cj0KCQiA2ITuBRDkARIsAMK9Q7PX0xVpwmSbsnSagH3HERI2a5R03okOnMsBEndkQs52Zg4GhqYA6YEaAgdbEALw_wcB:G:s&gclid=Cj0KCQiA2ITuBRDkARIsAMK9Q7PX0xVpwmSbsnSagH3HERI2a5R03okOnMsBEndkQs52Zg4GhqYA6YEaAgdbEALw_wcB">
       <img src="assests/images/sponsers/Citrix.png" alt="CITRIX" height="80" width="80" align="middle"> </a>
