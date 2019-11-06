b78726020fa95fa3280671af31b67b262fa8a09e
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-11-06 14:17:34 +0300

    Update right-panel.php

--- parts/right-panel.php
+++ parts/right-panel.php
@@ -1,44 +1,61 @@
 <div class="col-lg-3 col-sm-3">
   <h1> Sponsors</h1>
+  
   <ul class="" tyle="margin: 0 auto;
-      display: grid;
-      list-style: none;">
-        <li style="margin: 0 auto;
-      display: grid;
-      list-style: none;">
+   display: grid;
+   list-style: none;">
+  
+  <li style="margin: 0 auto; display: grid; list-style: none;">
+  <div class="slider-content-sub">         
+  <img src="assests/images/sponsers/cisco.svg" alt="" width="80px" height="80px"/>
+  </div>
+  </li>
+    
+   <li style="margin: 0 auto; display: grid; list-style: none;">
+  <div class="slider-content-sub">         
+  <img src="assests/images/sponsers/Citrix.png" alt="" width="80px" height="80px"/>
+  </div>
+  </li>
+    
+   <li style="margin: 0 auto; display: grid; list-style: none;">
+  <div class="slider-content-sub">         
+  <img src="assests/images/sponsers/hpe.png" alt="" width="80px" height="80px"/>
+  </div>
+  </li>
+    
+    <li style="margin: 0 auto; display: grid; list-style: none;">
+  <div class="slider-content-sub">         
+  <img src="assests/images/sponsers/NUTANIX00.png" alt="" width="80px" height="80px"/>
+  </div>
+  </li>
+    
+    
+      <li class="jqslider karma-slider-normal-image" style="margin: 0 auto; display: grid; list-style: none;">
+      <div class="slider-content-sub">
+      <img src="assests/images/sponsers/huawei-icon.svg" alt=""width="80px" height="80px" />
+      </div>
+      </li>
+    
+    
+ <li class="jqslider karma-slider-normal-image" style="margin: 0 auto; display: grid; list-style: none;">
+ <div class="slider-content-sub">
+ <img src="assests/images/sponsers/dellemc.svg" alt=""width="80px" height="80px" />
+ </div>
+    
+   
+      <div class="slider-content-sub">
+      <img src="assests/images/sponsers/aec.svg" alt=""width="80px" height="80px" />
+      </div>
+      <div class="slider-content-sub">
+      <img src="assests/images/sponsers/Orange_logo.svg" alt=""width="80px" height="80px" />
+      </div>
+      </li>
 
-           <div class="slider-content-sub">
-             
-              <img src="assests/images/sponsers/cisco.svg" alt="" width="80px" height="80px"/>
-           </div>
+    
+    
+   </ul>
+  
+  
 
-        </li>
-        <li class="jqslider karma-slider-normal-image" style="margin: 0 auto;
-      display: grid;
-      list-style: none;">
-
-           <div class="slider-content-sub">
-              <img src="assests/images/sponsers/huawei-icon.svg" alt=""width="80px" height="80px" />
-           </div>
-
-        </li>
-        <li class="jqslider karma-slider-normal-image" style="margin: 0 auto;
-      display: grid;
-      list-style: none;">
-
-           <div class="slider-content-sub">
-              <img src="assests/images/sponsers/dellemc.svg" alt=""width="80px" height="80px" />
-           </div>
-           <div class="slider-content-sub">
-             assests/images/sponsers/1.png
-              <img src="assests/images/sponsers/aec.svg" alt=""width="80px" height="80px" />
-           </div>
-           <div class="slider-content-sub">
-              <img src="assests/images/sponsers/Orange_logo.svg" alt=""width="80px" height="80px" />
-           </div>
-
-        </li>
-
-     </ul>
 </div>
 <br />
