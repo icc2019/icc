7741e908cf9317c1c5b358c36a0f604c69066f15
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-11-07 00:50:15 +0300

    Update Submission.php

--- Submission.php
+++ Submission.php
@@ -1,60 +1,37 @@
+<?php include('parts/head.php');?>
 
-<?php //include('right-panel.php');?>
-<div class="col">
-  <div class="Organizing-Committee">
-      
-    
-
-
+   <div id="tt-wide-layout" class="content-style-default">
+      <div id="wrapper">
+        <!-- ******************header****************** -->
+     <?php include('parts/header.php');?>
 
 
-<h1>WORKSHOP PROPOSAL SUBMISSION</h1>
-   
-    
-   
-    <p align="justify">
 
-      <b> You can submit a  workshop proposal as a single  PDF file online via the following email:<a href="mailto:ccis-cs-workshop@pnu.edu.sa"><strong>ccis-cs-workshop@pnu.edu.sa</strong></a> </b>
-    
-    </p>
-  
-    
-    <br> 
-  <table>
-     
-    <tr> 
-      <td> <p>Download the workshop proposal format </p> </td>
-      <td> <p> <b> 	&nbsp; 	&nbsp; &nbsp; 		&nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;	&nbsp;  &nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;  &nbsp;  &nbsp; 	&nbsp;	&nbsp;  &nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;  <a href="assests/Abstract_ [2702].docx" target="_blank"download> here </a> </b> </p> </td>
-    </tr>
-  
-  
-    <tr>
-     <td><p>The deadline for workshop abstract submission </p></td>
-      <td><p> <del> June 6th , 2019 </del> 	&nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;	&nbsp;  &nbsp; 	&nbsp; 	&nbsp;  &nbsp; 	&nbsp; June 30th, 2019</p></td>
-    </tr>
-    <tr>
-      <td><p>Announcement of acceptance/rejection </p></td>
-      <td><p> 	&nbsp; 	&nbsp; &nbsp; 		&nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;	&nbsp;  &nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;  &nbsp;  &nbsp; 	&nbsp;	&nbsp;  &nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;   July 15th, 2019</p></td>
-    </tr>
-    <tr>
-      <td><p>Deadline for final presentation submission</p></td>
-      <td><p> 	&nbsp; 	&nbsp; &nbsp; 		&nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;	&nbsp;  &nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;  &nbsp;  &nbsp; 	&nbsp;	&nbsp;  &nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;   August 10th, 2019</p></td>
-    </tr>
-  </table>
-   
-    <bR>
+         <!-- ***************** - Main Content Area - ***************** -->
+         <div id="main" class="tt-slider-karma-custom-jquery-1">
+            <div class="main-area">
 
-  <ul>
-  <ol><p><b>Please address all questions to the ICC Workshops’ Chairs:</b></p></ol>
-  <ol><p>Chair Dr. Norah Alghamdi</p></ol>
-  <ol><p>Deputy Chair Dr. Dua Nassar​</p></ol>
-  <ol><p>Contact <a href="mailto:ccis-cs-workshop@pnu.edu.sa"><strong>ccis-cs-workshop@pnu.edu.sa</strong></a></p></ol>
-  </ul>
-  </div>
+            <!-- END jquery1-slider-wrap -->
+        <?php include('parts/Submission.php');?>
+               <main role="main" id="content" class="">
 
-    </div>
-  </div>
+              </main><!-- END main #content -->
+
+            </div><!-- END main-area -->
+
+         <div id="footer-top">&nbsp;</div>
+      </div><!-- END main -->
 
 
-<?php //include('left-panel.php');?>
-<?php include('bottom-panel.php');?>
+
+
+         <!-- ***************** - Footer Starts Here - ***************** -->
+      <?php include('parts/footer.php');?>
+
+
+</div><!-- END wrapper -->
+</div><!-- END tt-layout -->
+
+
+<!-- *************foot**************** -->
+<?php include('parts/foot.php'); ?>
