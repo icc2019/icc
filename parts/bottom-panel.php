29acba8bccfda33dd0ea2d93631e4b10f6df91d5
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-11-06 15:07:57 +0300

    Update bottom-panel.php

--- parts/bottom-panel.php
+++ parts/bottom-panel.php
@@ -27,6 +27,15 @@
     &nbsp;
     &nbsp;
     &nbsp;
+      &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
     <a target="_blank" href="https://www.cisco.com/c/en_sa/index.html">
 <img src="assests/images/sponsers/cisco.svg" alt="CISCO" height="80" width="80" ali>
       &nbsp;
@@ -42,22 +51,7 @@
       <a target="_blank" href="https://www.nutanix.com/go/what-is-nutanix-hyperconverged-infrastructure?utm_source=google_adwords&utm_medium=paid_search&utm_campaign=Google_Search_DG_Alpha_Brand_SAU&utm_term=nutanix&gclid=Cj0KCQiA2ITuBRDkARIsAMK9Q7M-Lf_v2MToAxmKXGykI_XqJewLbxiGPOHfsoTG0V5a2x192rHTTIwaAg1TEALw_wcB">
     <img src="assests/images/sponsers/NUTANIX00.png" alt="NUTANIX" height="80" width="80"> </a>
       &nbsp;
-      
-      <a target="_blank" href="https://consumer.huawei.com/sa-en/">
-    <img src="assests/images/sponsers/huawei-icon.svg" alt="HUAWEI" height="80" width="80"> </a> 
-      &nbsp;
-      
-      <a target="_blank" href="https://www.dellemc.com/ar-sa/index.htm">
-    <img src="assests/images/sponsers/dellemc.svg" alt="DELL EMC" height="80" width="80"> </a>
-      &nbsp;
-      
-      <a target="_blank" href="http://www.aecl.com/en/Home">
-    <img src="assests/images/sponsers/aec.svg" alt="AEC" height="80" width="80"> </a>
-      &nbsp;
-      
-      <a target="_blank" href="https://www.orange-business.com/en">
-<img src="assests/images/sponsers/Orange_logo.svg" alt="Orange" height="80" width="80">
-    &nbsp;
+ 
     
     
     
