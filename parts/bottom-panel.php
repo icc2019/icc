32a24eaf456aa167cb9f92d8c678bd3739f3207f
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-11-06 14:49:21 +0300

    Update bottom-panel.php

--- parts/bottom-panel.php
+++ parts/bottom-panel.php
@@ -4,6 +4,22 @@
 <div class="container">
   <div class="row">
 
+    
+    <a target="_blank" href="https://www.cisco.com/c/en_sa/index.html">
+<img src="assests/images/sponsers/cisco.svg" alt="CISCO" height="80" width="80">
+      
+    <a target="_blank" href="https://www.citrix.com/about/what-does-citrix-do.html?utm_medium=Paid+Search+(SEM)&utm_source=sem-bra-brand-em-en-sea-go&utm_term=brand+evergreen&utm_content=brand-think&ef_id=Cj0KCQiA2ITuBRDkARIsAMK9Q7PX0xVpwmSbsnSagH3HERI2a5R03okOnMsBEndkQs52Zg4GhqYA6YEaAgdbEALw_wcB:G:s&gclid=Cj0KCQiA2ITuBRDkARIsAMK9Q7PX0xVpwmSbsnSagH3HERI2a5R03okOnMsBEndkQs52Zg4GhqYA6YEaAgdbEALw_wcB">
+      <img src="assests/images/sponsers/Citrix.png" alt="CITRIX" height="80" width="80"> </a>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    <!--
     <div class="col-lg-2 col-sm-2 col-xs-1">
      <img src="assests/images/sponsers/cisco.svg" alt="" width="80px" height="80px"/>
     </div>
@@ -41,8 +57,9 @@
     
     <div class="col-lg-2 col-sm-2 col-xs-1" align="center">
        <img src="assests/images/sponsers/Orange_logo.svg" alt=""width="80px" height="80px" />
-
     </div>
+
+-->
 
   </div>
 </div>
