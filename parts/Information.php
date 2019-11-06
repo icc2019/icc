dd3ba6bc019aec2fa273106d24bda702af0c0701
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-11-07 00:14:47 +0300

    Create Information.php

---
+++ parts/Information.php
@@ -0,0 +1,29 @@
+<?php //include('right-panel.php');?>
+<div class="col">
+  <div class="Organizing-Committee">
+    <h1>  Important Dates</h1>
+
+  <table>
+    <tr>
+      <td><p>Submission Deadline</p></td>
+      <td><p>June 20<sup>th</sup>, 2019 (Final Extension) </p></td>
+    </tr>
+    <tr>
+      <td><p>Notification Deadline</p></td>
+      <td><p>July 15<sup>th</sup>, 2019  </p></td>
+
+
+
+    </tr>
+    <tr>
+      <td><p>Camera Ready Deadline</p></td>
+      <td><p>	​August 1<sup>st</sup>, 2019​</p></td>
+
+    </tr>
+  </table>
+  <br/><br/><br/><br/><br/><br/><br/><br/><br/>
+  </div>
+
+</div>
+<?php //include('left-panel.php');?>
+<?php include('bottom-panel.php');?>
