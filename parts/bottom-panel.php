234fb38571488f4ca47c7ff0838bb379d100638e
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-11-06 14:26:57 +0300

    Update bottom-panel.php

--- parts/bottom-panel.php
+++ parts/bottom-panel.php
@@ -5,26 +5,42 @@
   <div class="row">
 
     <div class="col-lg-2 col-sm-2 col-xs-4">
-     <img src="assests/images/sponsers/huawei-icon.svg" alt="" width="400px" height="200px"/>
+     <img src="assests/images/sponsers/cisco.svg" alt="" width="80px" height="80px"/>
 
 
     </div>
 
     <div class="col-lg-2 col-sm-2 col-xs-4">
-       <img src="assests/images/sponsers/dellemc.svg" alt=""width="400px" height="200px" />
+       <img src="assests/images/sponsers/Citrix.png" alt=""width="400px" height="80px" />
     </div>
 
     <div class="col-lg-2 col-sm-2 col-xs-4">
-       <img src="assests/images/sponsers/aec.svg" alt=""width="400px" height="200px" />
+       <img src="assests/images/sponsers/hpe.png" alt=""width="80px" height="80px" />
     </div>
 
     <div class="col-lg-2 col-sm-2 col-xs-4">
-       <img src="assests/images/sponsers/cisco.svg" alt=""width="400px" height="200px" />
+       <img src="assests/images/sponsers/NUTANIX00.png" alt=""width="80px" height="80px" />
 
     </div>
 
     <div class="col-lg-2 col-sm-2 col-xs-4">
-       <img src="assests/images/sponsers/Orange_logo.svg" alt=""width="400px" height="200px" />
+       <img src="assests/images/sponsers/huawei-icon.svg" alt=""width="80px" height="80px" />
+
+    </div>
+    
+    <div class="col-lg-2 col-sm-2 col-xs-4">
+       <img src="assests/images/sponsers/dellemc.svg" alt=""width="80px" height="80px" />
+
+    </div>
+    
+    <div class="col-lg-2 col-sm-2 col-xs-4">
+       <img src="assests/images/sponsers/aec.svg" alt=""width="80px" height="80px" />
+
+    </div>
+   
+    
+    <div class="col-lg-2 col-sm-2 col-xs-4">
+       <img src="assests/images/sponsers/Orange_logo.svg" alt=""width="80px" height="80px" />
 
     </div>
 
