f5a4b299cfa6ee43a1e43ebd637cdbd79457485e
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-11-06 23:55:07 +0300

    Update Submission.php

--- Submission.php
+++ Submission.php
@@ -1,13 +1,27 @@
 <?php //include('right-panel.php');?>
 <div class="col">
+  <div class="Registration">
  
-
-
-
-   </div>
- 
-  <div class="">
-  
+ <!--   
+    <!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+</head>
       
     
 
