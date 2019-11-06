788df79f17a0ac6b1476b0f702fc066cc2039b98
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-11-06 14:44:59 +0300

    Update bottom-panel.php

--- parts/bottom-panel.php
+++ parts/bottom-panel.php
@@ -4,42 +4,42 @@
 <div class="container">
   <div class="row">
 
-    <div class="col-lg-2 col-sm-2 col-xs-4">
+    <div class="col-lg-2 col-sm-2 col-xs-1">
      <img src="assests/images/sponsers/cisco.svg" alt="" width="80px" height="80px"/>
     </div>
 
-    <div class="col-lg-2 col-sm-2 col-xs-4">
+    <div class="col-lg-2 col-sm-2 col-xs-1">
        <img src="assests/images/sponsers/Citrix.png" alt=""width="80px" height="80px" />
     </div>
 
-    <div class="col-lg-2 col-sm-2 col-xs-4">
+    <div class="col-lg-2 col-sm-2 col-xs-1">
        <img src="assests/images/sponsers/hpe.png" alt=""width="80px" height="80px" />
     </div>
 
-    <div class="col-lg-2 col-sm-2 col-xs-4">
+    <div class="col-lg-2 col-sm-2 col-xs-1">
        <img src="assests/images/sponsers/NUTANIX00.png" alt=""width="80px" height="80px" />
 
     </div>
 
-    <div class="col-lg-2 col-sm-2 col-xs-4">
+    <div class="col-lg-2 col-sm-2 col-xs-1">
        <img src="assests/images/sponsers/huawei-icon.svg" alt=""width="80px" height="80px" />
 
     </div>
     
-    <div class="col-lg-2 col-sm-2 col-xs-4">
+    <div class="col-lg-2 col-sm-2 col-xs-1">
        <img src="assests/images/sponsers/dellemc.svg" alt=""width="80px" height="80px" />
 
     </div>
     
     <br> <br>
     
-    <div class="col-lg-2 col-sm-2 col-xs-4" align="center">
+    <div class="col-lg-2 col-sm-2 col-xs-1" align="center">
        <img src="assests/images/sponsers/aec.svg" alt=""width="80px" height="80px" />
 
     </div>
    
     
-    <div class="col-lg-2 col-sm-2 col-xs-4" align="center">
+    <div class="col-lg-2 col-sm-2 col-xs-1" align="center">
        <img src="assests/images/sponsers/Orange_logo.svg" alt=""width="80px" height="80px" />
 
     </div>
