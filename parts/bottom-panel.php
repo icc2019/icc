7cfad4962a0ebe746b440039357c1b95d09d7cc2
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-11-06 14:52:12 +0300

    Update bottom-panel.php

--- parts/bottom-panel.php
+++ parts/bottom-panel.php
@@ -7,10 +7,35 @@
     
     <a target="_blank" href="https://www.cisco.com/c/en_sa/index.html">
 <img src="assests/images/sponsers/cisco.svg" alt="CISCO" height="80" width="80">
+      &nbsp;
       
     <a target="_blank" href="https://www.citrix.com/about/what-does-citrix-do.html?utm_medium=Paid+Search+(SEM)&utm_source=sem-bra-brand-em-en-sea-go&utm_term=brand+evergreen&utm_content=brand-think&ef_id=Cj0KCQiA2ITuBRDkARIsAMK9Q7PX0xVpwmSbsnSagH3HERI2a5R03okOnMsBEndkQs52Zg4GhqYA6YEaAgdbEALw_wcB:G:s&gclid=Cj0KCQiA2ITuBRDkARIsAMK9Q7PX0xVpwmSbsnSagH3HERI2a5R03okOnMsBEndkQs52Zg4GhqYA6YEaAgdbEALw_wcB">
       <img src="assests/images/sponsers/Citrix.png" alt="CITRIX" height="80" width="80"> </a>
-    
+      &nbsp;
+      
+      <a target="_blank" href="https://www.hpe.com/emea_europe/en/home.html">
+    <img src="assests/images/sponsers/hpe.png" alt="HPE" height="80" width="80"> </a>
+      &nbsp;
+      
+      <a target="_blank" href="https://www.nutanix.com/go/what-is-nutanix-hyperconverged-infrastructure?utm_source=google_adwords&utm_medium=paid_search&utm_campaign=Google_Search_DG_Alpha_Brand_SAU&utm_term=nutanix&gclid=Cj0KCQiA2ITuBRDkARIsAMK9Q7M-Lf_v2MToAxmKXGykI_XqJewLbxiGPOHfsoTG0V5a2x192rHTTIwaAg1TEALw_wcB">
+    <img src="assests/images/sponsers/NUTANIX00.png" alt="NUTANIX" height="80" width="80"> </a>
+      &nbsp;
+      
+      <a target="_blank" href="https://consumer.huawei.com/sa-en/">
+    <img src="assests/images/sponsers/huawei-icon.svg" alt="HUAWEI" height="80" width="80"> </a> 
+      &nbsp;
+      
+      <a target="_blank" href="https://www.dellemc.com/ar-sa/index.htm">
+    <img src="assests/images/sponsers/dellemc.svg" alt="DELL EMC" height="80" width="80"> </a>
+      &nbsp;
+      
+      <a target="_blank" href="http://www.aecl.com/en/Home">
+    <img src="assests/images/sponsers/aec.svg" alt="AEC" height="80" width="80"> </a>
+      &nbsp;
+      
+      <a target="_blank" href="https://www.orange-business.com/en">
+<img src="assests/images/sponsers/Orange_logo.svg" alt="Orange" height="80" width="80">
+    &nbsp;
     
     
     
