0b79052c541b4fa13756009c6019a5cbef019531
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-11-11 23:41:10 +0300

    Update Submission.php

--- parts/Submission.php
+++ parts/Submission.php
@@ -9,6 +9,198 @@
       
     </div>
     
+    
+    
+    
+    
+
+  <h1>
+  WORKSHOP </h1>
+
+   <p align="justify">
+
+  The International Conference on Computing (ICC) will host a number of workshops. Each workshop will be led by one presenter/group of presenters for the duration of the session time, which may be short, medium, or long. The aim of the conference workshops is to provide training sessions for emerging topics not specifically covered in the main symposia. Workshops should highlight trend topics related to Data Science, IoT, Cyber Security, and Networks. An invitation is open to submit workshop proposals.
+  </p><br />
+   <h1 >WORKSHOP TOPICS</h1>
+   <p align="justify">
+     <b>The topics for the workshops are under four trends as follows:</b>
+   </p>
+  <div class=" tt-column">
+                       <div class="tabs-area ui-tabs ui-widget ui-widget-content ui-corner-all">
+                          <ul class="tabset ui-tabs-nav ui-helper-reset ui-helper-clearfix ui-widget-header ui-corner-all" role="tablist">
+                             <li class="ui-state-default ui-corner-top   col-lg-2 col-xs-12 col-sm-12" role="tab" tabindex="-1" aria-controls="tab-3" aria-labelledby="ui-id-4" aria-selected="false"><a href="#tab-3" class="tab ui-tabs-anchor" role="presentation" tabindex="-1" id="ui-id-4"><span>Data Science</span></a></li>
+                             <li class="ui-state-default ui-corner-top col-lg-2 col-xs-12 col-sm-12" role="tab" tabindex="-1" aria-controls="tab-4" aria-labelledby="ui-id-5" aria-selected="false"><a href="#tab-4" class="tab ui-tabs-anchor" role="presentation" tabindex="-1" id="ui-id-5"><span>Cyber Security</span></a></li>
+
+                             <li class="ui-state-default ui-corner-top col-lg-2 col-xs-12 col-sm-12" role="tab" tabindex="0" aria-controls="tab-6" aria-labelledby="ui-id-7" aria-selected="true"><a href="#tab-6" class="tab ui-tabs-anchor" role="presentation" tabindex="-1" id="ui-id-7"><span>Internet of Things (IoT)</span></a></li>
+                          </ul>
+                          <div id="tab-3" class="tab-box ui-tabs-panel ui-widget-content ui-corner-bottom" aria-labelledby="ui-id-4" role="tabpanel" aria-expanded="false" aria-hidden="true" style="display: none;">
+                                   <p>
+                            &#9679; Deep Learning <br>
+                            &#9679; Machine Learning <br>
+                            &#9679; Predictive Analytics <br>
+                            &#9679; Natural Language Processing <br>
+                            &#9679; Data Visualization <br>
+                            &#9679; Data Wrangling <br>
+                            &#9679; AI Research <br>
+                            &#9679; Data Science at Scale <br>
+                            &#9679; Data Science Management <br>
+                            &#9679; Data Science Workflows <br>
+                            &#9679; Recommendation Systems <br>
+                            &#9679; Transfer Learning <br>
+                            &#9679; Conversational AI <br>
+                            &#9679; Speech Recognition <br>
+                            &#9679; Unsupervised Learning <br>
+                            &#9679; Image Classification <br>
+                            &#9679; Machine translation <br>
+                           
+                             </div> 
+                           
+                         <!--
+                            <p> <ul>
+                               <li> <p>Deep Learning</p></li>
+                               <li> <p>Machine Learning</p></li>
+                               <li> <p>Predictive Analytics</p></li>
+                               <li> <p>Natural Language Processing</p></li>
+                               <li> <p>Data Visualization</p></li>
+                               <li> <p>Data Wrangling</p></li>
+                               <li> <p>AI Research</p></li>
+                               <li> <p>Data Science at Scale</p></li>
+                               <li> <p>Data Science Management</p></li>
+                               <li> <p>Data Science Workflows</p></li>
+                               <li> <p>Recommendation Systems</p></li>
+                             <li> <p>Transfer Learning</p></li>
+                               <li> <p>Conversational AI</p></li>
+                               <li> <p>Speech Recognition</p></li>
+                               <li> <p>Unsupervised Learning</p></li>
+                               <li> <p>Image Classification</p></li>
+                               <li> <p>Machine translation</p></li>
+                               <li> <p>Deep Learning</p></li>
+                               <li> <p>Machine Learning</p></li>
+                               <li> <p>Predictive Analytics</p></li>
+                               <li> <p>Natural Language Processing</p></li>
+                               <li> <p>Data Visualization</p></li>
+                               <li> <p>Data Wrangling</p></li>
+                               <li> <p>AI Research</p></li>
+                               <li> <p>Data Science at Scale</p></li>
+                               <li> <p>Data Science Management</p></li>
+                               <li> <p>Data Science Workflows</p></li>
+                               <li> <p>Recommendation Systems</p></li>
+                               <li> <p>Transfer Learning</p></li>
+                               <li> <p>Conversational AI</p></li>
+                               <li> <p>Speech Recognition</p></li>
+                               <li> <p>Unsupervised Learning</p></li>
+                               <li> <p>Image Classification</p></li>
+                               <li> <p>Machine translation</p></li>
+                             </ul></p>
+-->
+
+                          
+                          <div id="tab-4" class="tab-box ui-tabs-panel ui-widget-content ui-corner-bottom" aria-labelledby="ui-id-5" role="tabpanel" aria-expanded="false" aria-hidden="true" style="display: none;">
+                           
+                              <p>
+                            &#9679; Authentication and Authorization <br>
+                            &#9679; Communication and Network Security <br>
+                            &#9679; Security Testing and Verification <br>
+                            &#9679; Socio-technical Security <br>
+                            &#9679; Reverse Engineering <br>
+                          
+                             </div> 
+                            
+                            <!--
+                            <p><ul>
+                               <li> <p>Authentication and Authorization</p></li>
+                               <li> <p>Communication and Network Security</p></li>
+                               <li> <p>Security Testing and Verification</p></li>
+                               <li> <p>Socio-technical Security</p></li>
+                               <li> <p>Reverse Engineering</p></li>
+                               <br/>
+                             </ul></p>
+                          </div>
+-->
+                            
+                          <div id="tab-5" class="tab-box ui-tabs-panel ui-widget-content ui-corner-bottom" aria-labelledby="ui-id-6" role="tabpanel" aria-expanded="false" aria-hidden="true" style="display: none;">
+                             
+                               <p>
+                            &#9679; Software-defined networks <br>
+                            &#9679; Network virtualization <br>
+                            &#9679; Vehicular and 5G networks <br>
+                            &#9679; Cloud networking <br>
+                            &#9679; Network data analytics <br>
+                            &#9679; Smart cities <br>
+                            &#9679; Wireless networks <br>
+                            &#9679; Mobile networks <br>
+                            &#9679; Satellite networks <br>
+                            &#9679; Optical networks <br>
+                            &#9679; Multimedia networks <br>
+                             
+                          
+                             </div> 
+                            
+                            <!--
+                            <p> <ul>
+                               <li> <p>Software-defined networks.</p></li>
+                               <li> <p>Network virtualization.</p></li>
+                               <li> <p>Vehicular and 5G networks.</p></li>
+                               <li> <p>Cloud networking.</p></li>
+                               <li> <p>Network data analytics.</p></li>
+                               <li> <p>Smart cities.</p></li>
+                               <li> <p>Wireless networks.</p></li>
+                               <li> <p>Mobile networks.</p></li>
+                               <li> <p>Satellite networks.</p></li>
+                               <li> <p>Optical networks.</p></li>
+                               <li> <p>Multimedia networks.</p></li>
+                               <br/>
+                             </ul></p>
+                          </div>
+-->
+                            
+                            
+                          <div id="tab-6" class="tab-box ui-tabs-panel ui-widget-content ui-corner-bottom" aria-labelledby="ui-id-7" role="tabpanel" aria-expanded="true" aria-hidden="false" style="display: block;">
+                            
+                             <p>
+                            &#9679; Internet of Things Architecture <br>
+                            &#9679; Big data and IoT Data Analytics <br>
+                            &#9679; IoT and Artificial Intelligence techniques <br>
+                            &#9679; Iot Best Practices, Standards, and Open-Source  <br>
+                            &#9679; Web Technologies for the IoT <br>
+                            &#9679; Description and Discovery of IoT devices, including mobile and moving devices <br>
+                            &#9679; Real World Applications of IoT technology <br>
+                            &#9679; Green IoT <br>
+                            &#9679; The Internet of People <br>
+                         
+                             </div> 
+                         
+                            <!--
+                            <p> <ul>
+                               <li> <p>Internet of Things Architecture</p></li>
+                               <li> <p>Big data and IoT Data Analytics </p></li>
+                               <li> <p>IoT and Artificial Intelligence techniques</p></li>
+                               <li> <p>Iot Best Practices, Standards, and Open-Source </p></li>
+                               <li> <p>Web Technologies for the IoT.</p></li>
+                               <li> <p>Description and Discovery of IoT devices, including mobile and moving devices.</p></li>
+                               <li> <p>Real World Applications of IoT technology</p></li>
+                               <li> <p>Green IoT</p></li>
+                               <li> <p>The Internet of People</p></li>
+                             </ul></p>
+                          </div>
+-->
+                            
+                            
+                       </div>
+                    </div>
+
+
+
+<!--
+  <h1>Please address all questions to the ICC Workshops Chairs:</p></h1>
+  <ul>
+    <ol><p> Chair Dr. Norah Alghamdi</p></ol>
+  <ol><p>Deputy Chair: Dr. Dua Nassar​</p></ol>
+  <ol><p>Contact: <a href="mailto:ccis-cs-workshop@pnu.edu.sa"><strong>ccis-cs-workshop@pnu.edu.sa</strong></a></p></ol>
+  </ul>
+-->
+    
+    
     <h1>WORKSHOP PROPOSAL SUBMISSION</h1>
     
    
