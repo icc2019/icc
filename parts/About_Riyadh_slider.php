53f3ee696398ec95c687c6df6f2ab2236931a73e
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-11-11 17:39:17 +0300

    Update About_Riyadh_slider.php

--- parts/About_Riyadh_slider.php
+++ parts/About_Riyadh_slider.php
@@ -126,7 +126,7 @@ img {vertical-align: middle;}
 
 <div class="mySlides fade">
   <div class="numbertext">1 / 6</div>
-  <img src="http://kingdomcentre.com.sa/wp-content/uploads/2015/01/KC-1.jpg" style="width:100%">
+  <img src="https://kingdomcentre.com.sa/docs/slider1.jpg" style="width:100%">
   <div class="text">Kingdom Centre Tower (Al-Mamlaka Tower)</div>
   
 </div>
