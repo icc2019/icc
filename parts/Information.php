32600dc74ca0482850affd3ac01ac9575f1c35e5
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-11-14 01:14:29 +0300

    Update Information.php

--- parts/Information.php
+++ parts/Information.php
@@ -1,6 +1,200 @@
 <?php //include('right-panel.php');?>
-<div class="col">
- 
+
+
+
+<div class="col" >
+  <!-- ============================================================================= -->
+  
+  
+  
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {box-sizing: border-box}
+body {font-family: Verdana, sans-serif; margin:0}
+.mySlides {display: none}
+img {vertical-align: middle;}
+/* Slideshow container */
+.slideshow-container {
+  max-width: 1000px;
+  position: relative;
+  margin: auto;
+}
+/* Next & previous buttons */
+.prev, .next {
+  cursor: pointer;
+  position: absolute;
+  top: 50%;
+  width: auto;
+  padding: 16px;
+  margin-top: -22px;
+  color: white;
+  font-weight: bold;
+  font-size: 18px;
+  transition: 0.6s ease;
+  border-radius: 0 3px 3px 0;
+  user-select: none;
+}
+/* Position the "next button" to the right */
+.next {
+  right: 0;
+  border-radius: 3px 0 0 3px;
+}
+/* On hover, add a black background color with a little bit see-through */
+.prev:hover, .next:hover {
+  background-color: rgba(0,0,0,0.8);
+}
+/* Caption text */
+.text {
+  color: #f2f2f2;
+  font-size: 15px;
+  padding: 8px 12px;
+  position: absolute;
+  bottom: 8px;
+  
+  width: 100%;
+  text-align: center;
+}
+/* Number text (1/3 etc) */
+.numbertext {
+  color: #f2f2f2;
+  font-size: 12px;
+  padding: 8px 12px;
+  position: absolute;
+  top: 0;
+}
+/* The dots/bullets/indicators */
+.dot {
+  cursor: pointer;
+  height: 15px;
+  width: 15px;
+  margin: 0 2px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+  transition: background-color 0.6s ease;
+}
+.active, .dot:hover {
+  background-color: #717171;
+}
+/* Fading animation */
+.fade {
+  -webkit-animation-name: fade;
+  -webkit-animation-duration: 1.5s;
+  animation-name: fade;
+  animation-duration: 1.5s;
+}
+@-webkit-keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+@keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+/* On smaller screens, decrease text size */
+@media only screen and (max-width: 300px) {
+  .prev, .next,.text {font-size: 11px}
+}
+</style>
+</head>
+<body>
+
+
+ <h1> About Riyadh</h1>
+<br>
+  <br>
+  <br>
+
+<div class="slideshow-container">
+
+<div class="mySlides fade">
+  <div class="numbertext">1 / 6</div>
+  <img src="assests/images/cisco card-1.png" width="1000px" height="450px">
+  <div class="text">cisco card</div>
+  
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">2 / 6</div>
+  <img src="https://www.arabnews.com/sites/default/files/styles/n_670_395/public/media/28/02/2013/p5a_0.jpg?itok=NUbTyBAP" width="1000px" height="450px">
+  <div class="text">Al Masmak Museum </div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">3 / 6</div>
+  <img src="https://www.cntravellerme.com/sites/default/files/styles/1920px_900px_wide_landscape/public/images/2019/10/03/Dirriyah-Park-Heritage-City-Riyadh-Saudi-Tourism-Opens-%281%29.jpg?itok=rS1VCbGH&c=2ec070ae480137b3a6a7262bef07acba" width="1000px" height="450px">
+  <div class="text">Al Bujairi Heritage Park </div>
+</div>
+
+
+<div class="mySlides fade">
+  <div class="numbertext">4 / 6</div>
+  <img src="https://i2.wp.com/www.sauditravel.com/wp-content/uploads/king-abdullah-park.jpg?w=580" width="1000px" height="450px">
+  <div class="text">King Abdullah Park</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">5 / 6</div>
+  <img src="https://cdn.expatwoman.com/s3fs-public/editorial/SaudiMuseum04-1650x1300.jpg" width="1000px" height="450px">
+  <div class="text">National Museum of Saudi Arabia </div>
+</div>
+
+
+<div class="mySlides fade">
+  <div class="numbertext">6 / 6</div>
+  <img src="https://i.pinimg.com/originals/d7/ed/2a/d7ed2aaa8cdff9419ab460acc3733661.jpg" width="1000px" height="450px">
+  <div class="text">Tuwaiq Mountains (The Edge of The World)</div>
+</div>
+
+<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+<a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+</div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span> 
+  <span class="dot" onclick="currentSlide(2)"></span> 
+  <span class="dot" onclick="currentSlide(3)"></span> 
+  <span class="dot" onclick="currentSlide(4)"></span> 
+  <span class="dot" onclick="currentSlide(5)"></span> 
+  <span class="dot" onclick="currentSlide(6)"></span> 
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     <h1>  Information</h1>
 
   <table>
