af1e0df2c2e4737ab8ec32ad63cc72b5e56c3979
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-11-14 03:36:18 +0300

    Update Information.php

--- parts/Information.php
+++ parts/Information.php
@@ -209,28 +209,28 @@ img {vertical-align: middle;}
      
     <tr> 
       
-    <td> <p>​  To download the PDF version of "cisco card" </p> </td> 
+    <td> <p>To download the PDF version of "cisco card"</p> </td> 
       <td><b><p>&nbsp; 	&nbsp; &nbsp; 		&nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;	&nbsp;  &nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;  &nbsp;  &nbsp; 	&nbsp;	&nbsp;  &nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;<a href="assests/cisco card.pdf"target="_blank"cisco card>click here</a></p></td></tr> 
-        <td> <b><p>​ To download the PDF version of "dhabia card" </p> </td>
-      <td> <p>&nbsp; 	&nbsp; &nbsp;		&nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;	&nbsp;  &nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;  &nbsp;  &nbsp; 	&nbsp;	&nbsp;  &nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;<a href="assests/dhabia card.pdf"target="_blank"dhabia card>click here</a></p></td>
+        <td> <p>To download the PDF version of "dhabia card"</p> </td>
+      <td> <b><p>&nbsp; 	&nbsp; &nbsp;		&nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;	&nbsp;  &nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;  &nbsp;  &nbsp; 	&nbsp;	&nbsp;  &nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;<a href="assests/dhabia card.pdf"target="_blank"dhabia card>click here</a></p></td>
    <br></tr>
-          <td> <p>​To download the PDF version of "Michael card" </p> </td>
+          <td> <p>To download the PDF version of "Michael card"</p> </td>
 
       <td> <p><b>&nbsp; 	&nbsp; &nbsp; 		&nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;	&nbsp;  &nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;  &nbsp;  &nbsp; 	&nbsp;	&nbsp;  &nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;<a href="assests/Michael card.pdf"target="_blank"Michael card>click here</a></p></td>
    <br></tr>
             <td> <p>To download the PDF version of "moussa card" </p> </td>
       <td> <p> <b>&nbsp; 	&nbsp; &nbsp; 		&nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;	&nbsp;  &nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;  &nbsp;  &nbsp; 	&nbsp;	&nbsp;  &nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;<a href="assests/moussa card.pdf"target="_blank"moussa card>click here</a> </p> </td>
    <br></tr>
-              <td> <p>To download the PDF version of" norah alnashwan- card"</p> </td>
+              <td> <p>To download the PDF version of"norah alnashwan- card"</p> </td>
 
       <td> <p> <b>&nbsp; 	&nbsp; &nbsp; 		&nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;	&nbsp;  &nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;  &nbsp;  &nbsp; 	&nbsp;	&nbsp;  &nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;<a href="assests/norah alnashwan- card.pdf"target="_blank"norah alnashwan- card>click here</a> </p> </td>
    <br></tr>
-              <td> <p> To download the PDF version of"Norma card"</p> </td>
+              <td> <p>To download the PDF version of"Norma card"</p> </td>
 
       <td> <p> <b>&nbsp; 	&nbsp; &nbsp; 		&nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;	&nbsp;  &nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;  &nbsp;  &nbsp; 	&nbsp;	&nbsp;  &nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;<a href="assests/Norma card.pdf	"target="_blank"Norma card>click here</a></p></td>
         <br></tr>
 
-<td> <p>​  To download the PDF version of"Workshops’ Schedule" </p> </td>
+<td> <p>To download the PDF version of"Workshops’ Schedule"</p> </td>
       <td> <p> <b>&nbsp; 	&nbsp; &nbsp; 		&nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;	&nbsp;  &nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;  &nbsp;  &nbsp; 	&nbsp;	&nbsp;  &nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;<a href="assests/images/Workshops’ Schedule.jpg	"target="_blank"GUIDE WORKSHOPS>click here</a></p></td>
     
 
