65dc2d346848ae99d8ea2ba5e4b88488d9b97012
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-11-22 15:36:38 +0300

    Update bottom-panel.php

--- parts/bottom-panel.php
+++ parts/bottom-panel.php
@@ -4,27 +4,150 @@
 <div class="container">
   <div class="row">
 
-<div style="margin: 0 auto; text-align: center">
-    <a target="_blank" href="https://www.cisco.com/c/en_sa/index.html"    id="cc_strinfo_song_tranceilfm" class="cc_streaminfo" style="margin: 0px; display:block;">
-<img src="assests/images/sponsers/cisco.svg" alt="CISCO" height="150" width="150"> </a> </div>
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+      &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+      &nbsp;
+      &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+     &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+      &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+      &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+      &nbsp;
+      &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    <a target="_blank" href="https://www.cisco.com/c/en_sa/index.html">
+<img src="assests/images/sponsers/cisco.svg" alt="CISCO" height="150" width="150"> </a>
       <br>
       <br>
-
-    <div style="margin: 0 auto; text-align: center">
-    <a target="_blank" href="https://www.citrix.com/about/what-does-citrix-do.html?utm_medium=Paid+Search+(SEM)&utm_source=sem-bra-brand-em-en-sea-go&utm_term=brand+evergreen&utm_content=brand-think&ef_id=Cj0KCQiA2ITuBRDkARIsAMK9Q7PX0xVpwmSbsnSagH3HERI2a5R03okOnMsBEndkQs52Zg4GhqYA6YEaAgdbEALw_wcB:G:s&gclid=Cj0KCQiA2ITuBRDkARIsAMK9Q7PX0xVpwmSbsnSagH3HERI2a5R03okOnMsBEndkQs52Zg4GhqYA6YEaAgdbEALw_wcB"    id="cc_strinfo_song_tranceilfm" class="cc_streaminfo" style="margin: 0px; display:block;">
-<img src="assests/images/sponsers/Citrix.png" alt="CITRIX" height="80" width="80" > </a> </div>
-    
-  
+      &nbsp;
+    &nbsp;
+    &nbsp;
+        &nbsp;
+   &nbsp;
+      &nbsp;
+      &nbsp;
+      &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+        &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+      &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+      &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+      &nbsp;
+      &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+      &nbsp;
+      &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    &nbsp;
+      
+    <a target="_blank" href="https://www.citrix.com/about/what-does-citrix-do.html?utm_medium=Paid+Search+(SEM)&utm_source=sem-bra-brand-em-en-sea-go&utm_term=brand+evergreen&utm_content=brand-think&ef_id=Cj0KCQiA2ITuBRDkARIsAMK9Q7PX0xVpwmSbsnSagH3HERI2a5R03okOnMsBEndkQs52Zg4GhqYA6YEaAgdbEALw_wcB:G:s&gclid=Cj0KCQiA2ITuBRDkARIsAMK9Q7PX0xVpwmSbsnSagH3HERI2a5R03okOnMsBEndkQs52Zg4GhqYA6YEaAgdbEALw_wcB">
+      <img src="assests/images/sponsers/Citrix.png" alt="CITRIX" height="80" width="80"> </a>
       &nbsp;
       
       <a target="_blank" href="https://www.hpe.com/emea_europe/en/home.html">
-    <img src="assests/images/sponsers/newHPE.png" alt="HPE" height="80" width="80" align="middle"> </a>
+    <img src="assests/images/sponsers/newHPE.png" alt="HPE" height="80" width="80" > </a>
       &nbsp;
       
       <a target="_blank" href="https://www.nutanix.com/go/what-is-nutanix-hyperconverged-infrastructure?utm_source=google_adwords&utm_medium=paid_search&utm_campaign=Google_Search_DG_Alpha_Brand_SAU&utm_term=nutanix&gclid=Cj0KCQiA2ITuBRDkARIsAMK9Q7M-Lf_v2MToAxmKXGykI_XqJewLbxiGPOHfsoTG0V5a2x192rHTTIwaAg1TEALw_wcB">
-    <img src="assests/images/sponsers/NUTANIX00.png" alt="NUTANIX" height="80" width="80" align="middle"> </a>
+    <img src="assests/images/sponsers/NUTANIX00.png" alt="NUTANIX" height="80" width="80" > </a>
       &nbsp;
  
+    
     
     
     
