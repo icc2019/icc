bcd6a76490d7b4a1f72622624142b64c0611c94c
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-11-24 20:21:15 +0300

    Update contact.php

--- parts/contact.php
+++ parts/contact.php
@@ -1,4 +1,26 @@
-<div class="">
+<div class="col">
+<div class="links">
+<div class="sidebar-widget">
+
+    <div class=" tt-column">
+  <ul class="">
+<ol>
+    <h1>Steering Committee </h1>
+<p>
+   
+ <br>Heba A. Kurdi, <b> King Saud University, KSA </b>
+     <br>Mai Alduailij, <b>Princess Nourah University, KSA</b>
+    <br> Maali Alabdulhafith, <b>Princess Nourah University, KSA</b>
+    <br>Hadil Ahmed Shaiba, <b> Princess Nourah University, KSA </b>
+ <br>Shiroq Al-Megren, <b> Massachusetts Institute of Technology (MIT), USA</b>
+ 
+</ul>
+</div>
+</div>
+</div>
+</div>
+    
+    <div class="">
     <p>For any inqiry please contact us</p>
     <p>(email: <a href="mailto:ccis-nw-Inquiry@pnu.edu.sa"><b>ccis-nw-Inquiry@pnu.edu.sa</b></a>)</p>
     <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
