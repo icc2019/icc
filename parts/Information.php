100794922161cc4211ba7d9026c2cb4775b1fdcf
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-12-03 23:22:33 +0300

    Update Information.php

--- parts/Information.php
+++ parts/Information.php
@@ -125,49 +125,59 @@ img {vertical-align: middle;}
 
 <div class="mySlides fade">
   <div class="numbertext">1 / 7</div>
- <img src="assests/images/moussa card-1new.png" width="600px" height="750px">
-  <div class="text">CISCO Card</div>
-  
+ <img src="assests/images/Workshops’ Schedule.jpg" width="600px" height="750px">
+  <div class="text">Workshops’ Schedule</div>
+ 
 </div>
 
 <div class="mySlides fade">
   <div class="numbertext">2 / 7</div>
-  <img src="assests/images/dhabia card-1.png" width="600px" height="750px">
-  <div class="text">Dr.Dhabia Card </div>
+  <img src="assests/images/Michael card-1.png" width="600px" height="750px">
+  <div class="text">Eng. Michael Card</div>
+   
+  
 </div>
 
 <div class="mySlides fade">
   <div class="numbertext">3 / 7</div>
- <img src="assests/images/Michael card-1.png" width="600px" height="750px">
-  <div class="text">Eng. Michael Card </div>
+ <img src="assests/images/norah alnashwan- card-1.png" width="600px" height="750px">
+  <div class="text"> Norah Alnashwan Card</div>
+   
+  
 </div>
 
 
 <div class="mySlides fade">
   <div class="numbertext">4 / 7</div>
-  <img src="assests/images/moussa card-1.png" width="600px" height="750px">
-  <div class="text">Dr.Moussa Card</div>
+  <img src=" assests/images/Norma card-1.png" width="600px" height="750px">
+  <div class="text"> AP Dr.Norma Card</div>
+ 
+  
 </div>
 
 <div class="mySlides fade">
   <div class="numbertext">5 / 7</div>
-   <img src="assests/images/norah alnashwan- card-1.png" width="600px" height="750px">
-  <div class="text">Norah Alnashwan Card</div>
+   <img src="assests/images/moussa card-1.png" width="600px" height="750px">
+  <div class="text">Dr.Moussa Card</div>
+  
+
 </div>
 
 
 <div class="mySlides fade">
   <div class="numbertext">6 / 7</div>
- <img src="assests/images/Norma card-1.png" width="600px" height="750px">
-  <div class="text">AP Dr.Norma Card</div>
+ <img src="assests/images/dhabia card-1.png" width="600px" height="750px">
+  <div class="text">Dr.Dhabia Card</div>
+   
+  
 </div>
   
     <div class="mySlides fade">
   <div class="numbertext">7 / 7</div>
-  <img src="assests/images/Workshops’ Schedule.jpg" width="600px" height="750px">
-  <div class="text">Workshops’ Schedule</div>
+  <img src="assests/images/moussa card-1new.png" width="600px" height="750px">
+  <div class="text"> CISCO Card</div>
 </div>
-
+  
 <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
 <a class="next" onclick="plusSlides(1)">&#10095;</a>
 
