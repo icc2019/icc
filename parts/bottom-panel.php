6dba85b8b25b1d72fde24fb0dde5c1aa409169ff
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-12-03 18:40:16 +0300

    Update bottom-panel.php

--- parts/bottom-panel.php
+++ parts/bottom-panel.php
@@ -140,6 +140,10 @@
     &nbsp;
     &nbsp;
     &nbsp;
+    &nbsp;
+      &nbsp;
+    &nbsp;
+      &nbsp;
       
     <a target="_blank" href="https://www.citrix.com/about/what-does-citrix-do.html?utm_medium=Paid+Search+(SEM)&utm_source=sem-bra-brand-em-en-sea-go&utm_term=brand+evergreen&utm_content=brand-think&ef_id=Cj0KCQiA2ITuBRDkARIsAMK9Q7PX0xVpwmSbsnSagH3HERI2a5R03okOnMsBEndkQs52Zg4GhqYA6YEaAgdbEALw_wcB:G:s&gclid=Cj0KCQiA2ITuBRDkARIsAMK9Q7PX0xVpwmSbsnSagH3HERI2a5R03okOnMsBEndkQs52Zg4GhqYA6YEaAgdbEALw_wcB">
       <img src="assests/images/sponsers/Citrix.png" alt="CITRIX" height="80" width="80"> </a>
@@ -200,6 +204,8 @@
     &nbsp;
     &nbsp;
     &nbsp;
+    &nbsp;
+      &nbsp;
     &nbsp;
       &nbsp;
     &nbsp;
