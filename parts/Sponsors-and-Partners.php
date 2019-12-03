4bdc8f5fc1db1c0c0720ecf8bbd14358a234fcdc
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-12-03 18:31:45 +0300

    Update Sponsors-and-Partners.php

--- parts/Sponsors-and-Partners.php
+++ parts/Sponsors-and-Partners.php
@@ -45,6 +45,18 @@
     &nbsp;
 &nbsp; 
 &nbsp;
+        
+        
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+    
+        <h1 style="color:black;"> Bronze Sponsor </h1>
+<br>
+<br>
  
 <a target="_blank" href="https://www.nutanix.com/go/what-is-nutanix-hyperconverged-infrastructure?utm_source=google_adwords&utm_medium=paid_search&utm_campaign=Google_Search_DG_Alpha_Brand_SAU&utm_term=nutanix&gclid=Cj0KCQiA2ITuBRDkARIsAMK9Q7M-Lf_v2MToAxmKXGykI_XqJewLbxiGPOHfsoTG0V5a2x192rHTTIwaAg1TEALw_wcB">
     <img src="assests/images/sponsers/NUTANIX00.png" alt="NUTANIX" height="150" width="150"> </a>
