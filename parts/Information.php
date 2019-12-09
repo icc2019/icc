3f81c8337a427f12c30ff4d081d25e6e1275fa57
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-12-09 15:59:02 +0300

    Update Information.php

--- parts/Information.php
+++ parts/Information.php
@@ -259,7 +259,7 @@ function showSlides(n) {
         <br></tr>
 
 <td> <p>To download the PDF version of"Workshops’ Schedule"</p> </td>
-      <td> <p> <b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 	&nbsp;	&nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;  &nbsp;  &nbsp; 	&nbsp;	&nbsp;  &nbsp; 	&nbsp; &nbsp;<a href="assests/WORSHOPS GUIDE.pdf	"target="_blank"GUIDE WORKSHOPS>click here</a></p></td>
+      <td> <p> <b> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 	&nbsp;	&nbsp; 	&nbsp; &nbsp; 	&nbsp;  &nbsp; 	&nbsp;  &nbsp;  &nbsp; 	&nbsp;	&nbsp;  &nbsp; 	&nbsp; &nbsp;<a href="assests/WORSHOPS GUIDE1(1).pdf	"target="_blank"GUIDE WORKSHOPS>click here</a></p></td>
     
 
 
