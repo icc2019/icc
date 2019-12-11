e8507dabc2155cb5c259a83f06aaae7918075713
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-12-11 16:55:45 +0300

    Update proceedings.php

--- parts/proceedings.php
+++ parts/proceedings.php
@@ -3,8 +3,8 @@
   <div class="content" >
     <h1>Proceedings</h1>
     
-    <h5>Advances in Data Science, Cyber Security and IT Applications </h5>
-<h5> First International Conference on Computing, ICC 2019, Riyadh, Saudi Arabia, December 10–12, 2019, Proceedings, Part I + II </h5>
+    <p> <b>Advances in Data Science, Cyber Security and IT Applications </b></p>
+<p> <b> First International Conference on Computing, ICC 2019, Riyadh, Saudi Arabia, December 10–12, 2019, Proceedings, Part I + II </b></p>
     <br>
     <br>
     <br>
