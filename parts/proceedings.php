dcd4ca3d2b75ca79386c0ecae0b18ceec3b02c98
icc2019 <49359562+icc2019@users.noreply.github.com>
2019-12-11 16:32:48 +0300

    Update proceedings.php

--- parts/proceedings.php
+++ parts/proceedings.php
@@ -1,23 +1,37 @@
 <?php //include('right-panel.php');?>
-
-
-
-<div class="col" >
-  <!-- ============================================================================= -->
+<div class="col">
+  <div class="content" >
+    <h1>Proceedings</h1>
+    
+    <h3>Advances in Data Science, Cyber Security and IT Applications </h3>
+<h3> First International Conference on Computing, ICC 2019, Riyadh, Saudi Arabia, December 10–12, 2019, Proceedings, Part I + II </h3>
+    
+    <p align="justify">
+      <strong>Part I:</strong>
+      <br>
+ 
+CCIS 1097, ICC 2019:  Advances in Data Science, Cyber Security and IT Applications
+eISBN: 978-3-030-36365-9
+      <br>
+Conference website: http://www.icconf.org/
+      <br>
+SpringerLink: https://link.springer.com/book/10.1007/978-3-030-36365-9 
+ </p>
+    
+   <p align="justify"> 
+     <strong> Part II: </strong>
+ <br>
+CCIS 1098, ICC 2019:  Advances in Data Science, Cyber Security and IT Applications
+eISBN: 978-3-030-36368-0
+     <br>
+Conference website: http://www.icconf.org/
+     <br>
+SpringerLink: https://link.springer.com/book/10.1007/978-3-030-36368-0 
+    </p>
   
-  
-
- <h1> Proceedings </h1>
-
-       
-
-
-
+  </div>
 
 </div>
-
-
-
-
 <?php //include('left-panel.php');?>
 <?php include('bottom-panel.php');?>
+
